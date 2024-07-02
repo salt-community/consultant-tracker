3b8d19f318f268b41207d02a555f537907b9a30c
Mauro Morales <mauroale2501@gmail.com>
2024-07-02 15:34:54 +0200

    add mockdata and types

---
+++ src/mockData.ts
@@ -0,0 +1,12 @@
+export const navLinks = [
+  "Create new consultant",
+  "Create new client",
+  "Sign out",
+];
+
+export const infographicData = [
+  { title: "Total consultants", amount: 30, variant: "blue" },
+  { title: "Active consultants", amount: 28, variant: "green" },
+  { title: "Absent consultants", amount: 2, variant: "yellow" },
+  { title: "Number of clients", amount: 10, variant: "violet" },
+];
