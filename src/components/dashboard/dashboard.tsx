7d61ccb2f5366d769027e480807fa75264789486
kokakarova <kostadinkakarova@gmail.com>
2024-07-02 16:00:28 +0200

    add filter for the table by name, status, client

--- src/components/dashboard/dashboard.tsx
+++ src/components/dashboard/dashboard.tsx
@@ -1,15 +1,39 @@
+"use client"
 import Infographic from "./infographic/infographic";
 import "./dashboard.css";
 import { infographicData } from "@/mockData";
+import { ChangeEvent, useState } from "react";
 
 const Dashboard = () => {
+
+  const [filterValue, setFilterValue] = useState("");
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setFilterValue(e.target.value);
+    console.log("filter: ", filterValue)
+  }
   return (
-    <section >
+    <section>
       <div className="dashboard-infographic__card">
         {infographicData.map((element, index) => {
           const { title, amount, variant } = element;
-          return <Infographic key={index} title={title} amount={amount} variant={variant} />;
+          return (
+            <Infographic
+              key={index}
+              title={title}
+              amount={amount}
+              variant={variant}
+            />
+          );
         })}
+      </div>
+      <div className="dashboard-table__wrapper">
+      <input
+        type="text"
+        value={filterValue}
+        onChange={(e) => handleInputChange(e)}
+        placeholder='Type to filter by ...'
+      />
       </div>
     </section>
   );
