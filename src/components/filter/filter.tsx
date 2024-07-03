5eac8fa28470161730c87aeb0af5c25005682ae3
kokakarova <kostadinkakarova@gmail.com>
2024-07-03 11:37:14 +0200

    coordinate the filtering between the three criteria

--- src/components/filter/filter.tsx
+++ src/components/filter/filter.tsx
@@ -8,24 +8,62 @@ function FilterField() {
   const [filterValue, setFilterValue] = useState("");
   const [filterBy, setFilterBy] = useState("Consultant");
   const [data, setData] = useState<ConsultantDataType[]>(consultantsData);
+  const [status, setStatus] = useState("show all");
+  const [filteredDataState, setFilteredDataState] = useState<
+    ConsultantDataType[]
+  >([]);
+
+  const checkFilterValue = (dataVal: string, filterVal: string) => {
+    return dataVal.toLowerCase().includes(filterVal.toLowerCase());
+  };
+
+  const checkStatus = (dataVal: string, statusVal: string) => {
+    if (statusVal === "show all") {
+      console.log("checkStatus dataVal: ", dataVal);
+      console.log("checkStatus status: ", statusVal);
+      return true;
+    }
+    console.log("checkStatus dataVal: ", dataVal);
+    console.log("checkStatus status: ", statusVal);
+    return dataVal === statusVal;
+  };
+
+  const filterData = (searchParam: string, selectedStatus: string) => {
+    const filteredData = [...data].filter((df) => {
+      if (filterBy === "Consultant") {
+        return (
+          checkFilterValue(df.name, searchParam) &&
+          checkStatus(df.status, selectedStatus)
+        );
+      }
+      return (
+        checkFilterValue(df.client, searchParam) &&
+        checkStatus(df.status, selectedStatus)
+      );
+    });
+    setFilteredDataState(filteredData);
+    console.log("filteredData: ", filteredData);
+  };
 
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     setFilterValue(e.target.value);
-    const filteredData = [...data].filter((df) => {
-      if (filterBy === "Consultant") {
-        return df.name.toLowerCase().includes(e.target.value.toLowerCase());
-      }
-      return df.client.toLowerCase().includes(e.target.value.toLowerCase());
-    });
+    filterData(e.target.value, status);
   };
 
-  const handleSelection = (e: ChangeEvent<HTMLInputElement>) => {
+  const handleCriteriaSelection = (e: ChangeEvent<HTMLInputElement>) => {
     setFilterBy(e.target.value);
+    handleClear();
   };
 
   const handleClear = () => {
     setFilterValue("");
-  }
+    setStatus("show all");
+  };
+
+  const handleStatusChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    setStatus(e.target.value);
+    filterData(filterValue, e.target.value);
+  };
 
   return (
     <>
@@ -40,7 +78,7 @@ function FilterField() {
           type="radio"
           value="Consultant"
           name="filter"
-          onChange={(e) => handleSelection(e)}
+          onChange={(e) => handleCriteriaSelection(e)}
           checked={filterBy === "Consultant"}
         />
         Consultant
@@ -48,18 +86,27 @@ function FilterField() {
           type="radio"
           value="Client"
           name="filter"
-          onChange={(e) => handleSelection(e)}
+          onChange={(e) => handleCriteriaSelection(e)}
           checked={filterBy === "Client"}
         />
         Client
       </div>
+      <div>
+        <label htmlFor="status">Consultant status</label>
+        <select
+          name="status"
+          id="status"
+          onChange={(e) => handleStatusChange(e)}
+        >
+          {statusOptions.map((st, index) => (
+            <option value={st.toLowerCase()} key={index}>
+              {st}
+            </option>
+          ))}
+        </select>
+      </div>
       {/* <button>Search</button> */}
       <button onClick={handleClear}>Clear Filter</button>
-      <div>
-      <select name="status" id="status">
-        {statusOptions.map((st, index) => <option value={st.toLowerCase()} key={index}>{st}</option>)}
-      </select>
-      </div>
     </>
   );
 }
