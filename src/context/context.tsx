84224ed52e852095f333a1e9ad4f7eebbd23d4ca
Mauro Morales <mauroale2501@gmail.com>
2024-07-03 16:38:48 +0200

    add context and context provider in layout

---
+++ src/context/context.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { consultantsData } from "@/mockData";
+import { ConsultantDataType } from "@/types";
+import {
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  createContext,
+  useContext,
+  useState,
+} from "react";
+
+type TableContextType = {
+  data: ConsultantDataType[];
+  setData: Dispatch<SetStateAction<ConsultantDataType[]>>;
+};
+
+const TableContext = createContext<TableContextType>({data: [], setData: ()=>[...consultantsData]});
+
+type ContextProviderProps = {
+  children: ReactNode;
+};
+
+export const TableContextProvider = ({ children }: ContextProviderProps) => {
+  const [data, setData] = useState<ConsultantDataType[]>(consultantsData);
+  const value = { data, setData };
+
+  return <TableContext.Provider value={value}>{children}</TableContext.Provider>
+  ;
+};
+export const useTableContext = () => useContext(TableContext);
