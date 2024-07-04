b08d5e7eb4bf7e541715def544e61bd23aa5a8e1
kokakarova <kostadinkakarova@gmail.com>
2024-07-04 11:31:39 +0200

    deleted old table component

--- src/components/dashboard/dashboard.tsx
+++ src/components/dashboard/dashboard.tsx
@@ -1,18 +1,17 @@
 import Infographic from "./infographic/infographic";
 import "./dashboard.css";
-import {infographicData} from "@/mockData";
+import { infographicData } from "@/mockData";
 import FilterField from "../filter/filter";
 import PageWrapper from "@/components/page-wrapper/page-wrapper";
 import TableLegend from "@/components/table/table-legend/table-legend";
-import Table from "../table/table";
-import EnhancedTable from "../table/table-mui";
+import EnhancedTable from "../table/table";
 
 const Dashboard = () => {
   return (
     <PageWrapper>
       <div className="dashboard-infographic__card">
         {infographicData.map((element, index) => {
-          const {title, amount, variant} = element;
+          const { title, amount, variant } = element;
           return (
             <Infographic
               key={index}
@@ -23,9 +22,8 @@ const Dashboard = () => {
           );
         })}
       </div>
-      <FilterField/>
+      <FilterField />
       <TableLegend />
-      {/* <Table/> */}
       <EnhancedTable />
     </PageWrapper>
   );
--- src/components/table/table.tsx
+++ src/components/table/table.tsx
@@ -1,42 +1,245 @@
 "use client";
-
-import Link from "next/link";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
+import TableSortLabel from "@mui/material/TableSortLabel";
+import Paper from "@mui/material/Paper";
+import { visuallyHidden } from "@mui/utils";
+import { headCells } from "@/mockData";
+import { ConsultantDataType } from "@/types";
 import Indicator from "./table-legend/indicator/indicator";
 import { useTableContext } from "@/context/context";
+import Link from "next/link";
+import "./table.css";
 
-const Table = () => {
-  const data = useTableContext();
+function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+type Order = "asc" | "desc";
+
+function getComparator<Key extends keyof any>(
+  order: Order,
+  orderBy: Key
+): (
+  a: { [key in Key]: number | string },
+  b: { [key in Key]: number | string }
+) => number {
+  return order === "desc"
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+function stableSort<T>(
+  array: readonly T[],
+  comparator: (a: T, b: T) => number
+) {
+  const stabilizedThis = array.map((el, index) => [el, index] as [T, number]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) {
+      return order;
+    }
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map((el) => el[0]);
+}
+
+interface EnhancedTableProps {
+  numSelected: number;
+  onRequestSort: (
+    event: React.MouseEvent<unknown>,
+    property: keyof ConsultantDataType
+  ) => void;
+  onSelectAllClick: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  order: Order;
+  orderBy: string;
+  rowCount: number;
+}
+
+function EnhancedTableHead(props: EnhancedTableProps) {
+  const { order, orderBy, onRequestSort } = props;
+  const createSortHandler =
+    (property: keyof ConsultantDataType) =>
+    (event: React.MouseEvent<unknown>) => {
+      onRequestSort(event, property);
+    };
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Client</th>
-          <th>Details</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.data.map((item, index) => {
-          const { id, name, client, status } = item;
-          return (
-            <tr key={index}>
-              <td>
-                <Indicator value={status} />
-                {name}
-              </td>
-              <td>{client}</td>
-              <td>
-                <Link href={`/details/${id}`}>
-                  <img src="details.svg" alt="details-icon" />
-                </Link>
-              </td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+    <TableHead>
+      <TableRow>
+        {headCells.map((headCell) => (
+          <TableCell
+            key={headCell.id}
+            align={headCell.numeric ? "right" : "left"}
+            padding={headCell.disablePadding ? "none" : "normal"}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : "asc"}
+              onClick={createSortHandler(headCell.id)}
+            >
+              {headCell.label}
+              {orderBy === headCell.id ? (
+                <Box component="span" sx={visuallyHidden}>
+                  {order === "desc" ? "sorted descending" : "sorted ascending"}
+                </Box>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
   );
-};
+}
 
-export default Table;
+export default function EnhancedTable() {
+  const [order, setOrder] = React.useState<Order>("asc");
+  const [orderBy, setOrderBy] =
+    React.useState<keyof ConsultantDataType>("name");
+  const [selected, setSelected] = React.useState<readonly string[]>([]);
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  const tableData = useTableContext().data;
+
+  const handleRequestSort = (
+    event: React.MouseEvent<unknown>,
+    property: keyof ConsultantDataType
+  ) => {
+    const isAsc = orderBy === property && order === "asc";
+    setOrder(isAsc ? "desc" : "asc");
+    setOrderBy(property);
+  };
+
+  const handleSelectAllClick = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.checked) {
+      const newSelected = tableData.map((n) => n.id);
+      setSelected(newSelected);
+      return;
+    }
+    setSelected([]);
+  };
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const isSelected = (id: string) => selected.indexOf(id) !== -1;
+
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - tableData.length) : 0;
+
+  const visibleRows = React.useMemo(
+    () =>
+      stableSort(tableData, getComparator(order, orderBy)).slice(
+        page * rowsPerPage,
+        page * rowsPerPage + rowsPerPage
+      ),
+    [order, orderBy, page, rowsPerPage, tableData]
+  );
+
+  return (
+    <>
+    <p className="table-total-results">Total results: {tableData.length}</p>
+      <Box sx={{ width: "100%" }}>
+        <Paper sx={{ width: "100%", mb: 2 }}>
+          <TableContainer>
+            <Table
+              sx={{ minWidth: 750 }}
+              aria-labelledby="tableTitle"
+              size={"medium"}
+            >
+              <EnhancedTableHead
+                numSelected={selected.length}
+                order={order}
+                orderBy={orderBy}
+                onSelectAllClick={handleSelectAllClick}
+                onRequestSort={handleRequestSort}
+                rowCount={tableData.length}
+              />
+              <TableBody>
+                {visibleRows.map((row) => {
+                  const isItemSelected = isSelected(row.id);
+
+                  return (
+                    <TableRow
+                      hover
+                      aria-checked={isItemSelected}
+                      tabIndex={-1}
+                      key={row.id}
+                      selected={isItemSelected}
+                    >
+                      <TableCell style={{ width: "10px" }} align="center">
+                        <Indicator value={row.status} />
+                      </TableCell>
+                      <TableCell align="left">{row.name}</TableCell>
+                      <TableCell align="left">
+                        {row.client === "-" ? (
+                          row.client
+                        ) : (
+                          <Link
+                            href={`/clients/${row.client}`}
+                            className="clients-link"
+                          >
+                            {row.client}
+                          </Link>
+                        )}
+                      </TableCell>
+                      <TableCell align="center" style={{ width: "20px" }}>
+                        <Link
+                          href={`/consultants/${row.id}`}
+                          className="details-link"
+                        >
+                          <img src={row.details} alt="details-icon" />
+                        </Link>
+                      </TableCell>
+                    </TableRow>
+                  );
+                })}
+                {emptyRows > 0 && (
+                  <TableRow
+                    style={{
+                      height: 53 * emptyRows,
+                    }}
+                  >
+                    <TableCell colSpan={6} />
+                  </TableRow>
+                )}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <TablePagination
+            rowsPerPageOptions={[5, 10, 25]}
+            component="div"
+            count={tableData.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+          />
+        </Paper>
+      </Box>
+    </>
+  );
+}
