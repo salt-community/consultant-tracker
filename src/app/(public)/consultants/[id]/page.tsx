bb91ce137c928763e255de0400fa508fe3687a85
Mauro Morales <mauroale2501@gmail.com>
2024-07-09 08:38:52 +0200

    extract breadcrums and create card-detail

--- src/app/(public)/consultants/[id]/page.tsx
+++ src/app/(public)/consultants/[id]/page.tsx
@@ -1,42 +1,17 @@
-"use client";
-import Client from "@/components/consultant-detail/client/client";
-import Schedule from "@/components/consultant-detail/schedule/schedule";
 import "./page.css";
-import React, { SyntheticEvent, useState } from "react";
-import VacationInfo from "@/components/consultant-detail/vacation-info/vacation-info";
-import TabsComponent from "@/components/tabs/tabs";
-import { useDetailsContext } from "@/context/details";
 import BasicInfo from "@/components/consultant-detail/basic-info/basic-info";
 import BreadcrumbsComponent from "@/components/breadcrumbs/breadcrumbs";
+import CardDetails from "@/components/card-details/card-details";
 
 const ConsultantDetail = () => {
-  const [value, setValue] = useState("clients");
-
-  const handleChange = (event: SyntheticEvent, newValue: string) => {
-    setValue(newValue);
-  };
-  const details = useDetailsContext();
-  const content = () => {
-    switch (value) {
-      case "schedule":
-        return <Schedule />;
-      case "vacation":
-        return <VacationInfo />;
-      default:
-        return <Client />;
-    }
-  };
   return (
     <>
-      <BreadcrumbsComponent current={`${details.name}`} />
+      <BreadcrumbsComponent />
       <div className="detail-page__wrapper">
         <section className="section-consultants">
           <div className="detail-page__right-side__wrapper">
             <BasicInfo />
-            <div className="detail-page__tabs-wrapper">
-              <TabsComponent value={value} handleChange={handleChange} />
-              <div className="consultant-detail__card">{content()}</div>
-            </div>
+            <CardDetails/>
           </div>
         </section>
       </div>
--- src/components/breadcrumbs/breadcrumbs.tsx
+++ src/components/breadcrumbs/breadcrumbs.tsx
@@ -1,19 +1,24 @@
-import React from 'react';
-import Typography from '@mui/material/Typography';
-import Breadcrumbs from '@mui/material/Breadcrumbs';
-import Link from 'next/link';
+"use client";
 
-type Props = {
-  current: string
-}
+import React, { useEffect } from "react";
+import Typography from "@mui/material/Typography";
+import Breadcrumbs from "@mui/material/Breadcrumbs";
+import Link from "next/link";
+import { useDetailsContext } from "@/context/details";
 
-const BreadcrumbsComponent = ({current}: Props) => {
+const BreadcrumbsComponent = () => {
+  const details = useDetailsContext();
+  let name = details.name;
+  useEffect(() => {
+    name = details.name;
+  }, [details.name]);
+
   return (
     <Breadcrumbs aria-label="breadcrumb">
       <Link color="inherit" href="/">
         Home
       </Link>
-      <Typography color="text.primary">{current}</Typography>
+      <Typography color="text.primary">{name}</Typography>
     </Breadcrumbs>
   );
 };
