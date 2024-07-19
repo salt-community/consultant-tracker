d69af9da8f8a357fb4524bb2718ddc9e421d50c3
kokakarova <kostadinkakarova@gmail.com>
2024-07-19 12:17:20 +0200

    calculate remaing time

--- frontend/src/components/client-detail/consultants-list/consultants-list.tsx
+++ frontend/src/components/client-detail/consultants-list/consultants-list.tsx
@@ -14,13 +14,36 @@ const ConsultantsList = () => {
   const client = useClientsContext();
   const idParam = usePathname().split("/").pop();
   const singleClient = client.data.filter((el) => el.id === idParam)[0];
-  const [consultants, setConsultants] = useState<ConsultantsCalendarType[]>(consultantsCalendar);
   const [items, setItems] = useState<ConsultantItemsType[]>(consultantItems);
+
+  const getDatesForRemainingTime = (id: number) => {
+    const matchingDates = items.filter(i => i.group === id).map(d => d.start_time);
+    const workedTime = matchingDates.length * 8;
+    const remainingHours = 48 - workedTime;
+    const remainingDays = remainingHours / 8;
+    const lastWorkedDay = new Date(matchingDates[matchingDates.length - 1])
+
+    const startDate = new Date(lastWorkedDay);
+    startDate.setDate(lastWorkedDay.getDate() + 1)
+
+    const endDate = new Date(startDate);
+    endDate.setDate(startDate.getDate() + remainingDays);
+    endDate.setUTCHours(21,59,59,999);
+
+    return {
+      start_date: startDate,
+      end_date: endDate,
+      remainingHours: remainingHours
+    };
+  }
+
+  console.table("addEstimatedTimeLeft: " + getDatesForRemainingTime(1));
+
   return (
     <div>
       <div>
         <Timeline
-          groups={consultants}
+          groups={consultantsCalendar}
           items={items}
           defaultTimeStart={moment().add(-17, "day")}
           defaultTimeEnd={moment().add(4, "day")}
