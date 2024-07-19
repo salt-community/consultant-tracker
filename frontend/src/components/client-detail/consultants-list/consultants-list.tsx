cca3f3533d87ef2220a7938c52a0a0a1d87b3a1a
kokakarova <kostadinkakarova@gmail.com>
2024-07-19 11:20:43 +0200

    removed unused code

--- frontend/src/components/client-detail/consultants-list/consultants-list.tsx
+++ frontend/src/components/client-detail/consultants-list/consultants-list.tsx
@@ -4,51 +4,26 @@ import { usePathname } from "next/navigation";
 import Timeline from "react-calendar-timeline";
 import "react-calendar-timeline/lib/Timeline.css";
 import moment from "moment";
-import { consultantDataCalendar, consultantItems } from "@/mockData";
+import { consultantsCalendar, consultantItems } from "@/mockData";
+import { useState } from "react";
+import { ConsultantItemsType, ConsultantsCalendarType } from "@/types";
 
-const groups = [
-  { id: 1, title: "group 1" },
-  { id: 2, title: "group 2" },
-];
 
 const ConsultantsList = () => {
-  const items = [
-    {
-      id: 1,
-      group: 1,
-      title: "item 1",
-      start_time: moment(),
-      end_time: moment().add(1, "hour"),
-    },
-    {
-      id: 2,
-      group: 2,
-      title: "item 2",
-      start_time: moment().add(-0.5, "hour"),
-      end_time: moment().add(0.5, "hour"),
-    },
-    {
-      id: 3,
-      group: 1,
-      title: "item 3",
-      start_time: moment().add(2, "hour"),
-      end_time: moment().add(3, "hour"),
-    },
-  ];
 
   const client = useClientsContext();
   const idParam = usePathname().split("/").pop();
   const singleClient = client.data.filter((el) => el.id === idParam)[0];
-
-
+  const [consultants, setConsultants] = useState<ConsultantsCalendarType[]>(consultantsCalendar);
+  const [items, setItems] = useState<ConsultantItemsType[]>(consultantItems);
   return (
     <div>
       <div>
         <Timeline
-          groups={consultantDataCalendar}
-          items={consultantItems}
-          defaultTimeStart={moment().add(-12, "hour")}
-          defaultTimeEnd={moment().add(12, "hour")}
+          groups={consultants}
+          items={items}
+          defaultTimeStart={moment().add(-17, "day")}
+          defaultTimeEnd={moment().add(4, "day")}
         
         />
       </div>
