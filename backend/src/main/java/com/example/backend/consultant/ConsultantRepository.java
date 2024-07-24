ad063c7148f9fdfeb4e2644ae84d26b1236aa8a2
Mauro Morales <mauroale2501@gmail.com>
2024-07-24 16:02:59 +0200

    add Pageable in repository

--- backend/src/main/java/com/example/backend/consultant/ConsultantRepository.java
+++ backend/src/main/java/com/example/backend/consultant/ConsultantRepository.java
@@ -1,5 +1,7 @@
 package com.example.backend.consultant;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,7 +11,10 @@ import java.util.UUID;
 @Repository
 public interface ConsultantRepository extends JpaRepository<Consultant, UUID> {
     boolean existsByTimekeeperId(Long id);
+
     Consultant save(Consultant consultant);
+
+    Page<Consultant> findAllByActiveTrue(Pageable pageable);
 
     List<Consultant> findAllByActiveTrue();
 }
