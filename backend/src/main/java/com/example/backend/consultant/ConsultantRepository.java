d29fbf798806c401dbcfc80349385c4011c6e746
Mauro Morales <mauroale2501@gmail.com>
2024-07-24 12:31:15 +0200

    add findByactive to consultantrepository

--- backend/src/main/java/com/example/backend/consultant/ConsultantRepository.java
+++ backend/src/main/java/com/example/backend/consultant/ConsultantRepository.java
@@ -3,10 +3,13 @@ package com.example.backend.consultant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ConsultantRepository extends JpaRepository<Consultant, UUID> {
     boolean existsByTimekeeperId(Long id);
     Consultant save(Consultant consultant);
+
+    List<Consultant> findAllByActiveTrue();
 }
