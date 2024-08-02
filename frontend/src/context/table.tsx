68594ffac027d1bd35b4f0cb8ffb3a1eaf6a8542
Mauro Morales <mauroale2501@gmail.com>
2024-08-02 11:34:15 +0200

    add dashboardData in context

--- frontend/src/context/table.tsx
+++ frontend/src/context/table.tsx
@@ -1,6 +1,6 @@
 "use client";
-import { consultantsData } from "@/mockData";
-import { ConsultantDataType } from "@/types";
+import { ConsultantCalendarType } from "@/types";
+import dayjs, { Dayjs } from "dayjs";
 import {
   Dispatch,
   ReactNode,
@@ -11,21 +11,55 @@ import {
 } from "react";
 
 type TableContextType = {
-  data: ConsultantDataType[];
-  setData: Dispatch<SetStateAction<ConsultantDataType[]>>;
+  data: ConsultantCalendarType;
+  setData: Dispatch<SetStateAction<ConsultantCalendarType>>;
 };
 
-const TableContext = createContext<TableContextType>({data: [...consultantsData], setData: ()=>[...consultantsData]});
+const initialData = {
+  pageNumber: 0,
+  totalPages: 0,
+  totalConsultants: 0,
+  consultants: [
+    {
+      id: "",
+      fullName: "",
+      email: "",
+      phoneNumber: "",
+      totalDaysStatistics: {
+        totalRemainingDays: 0,
+        totalWorkedDays: 0,
+        totalVacationDaysUsed: 0,
+      },
+      registeredTimeDtoList: [
+        {
+          registeredTimeId: "",
+          startDate: dayjs(),
+          endDate: dayjs(),
+          type: "",
+          projectName: "",
+        },
+      ],
+    },
+  ],
+};
+
+const TableContext = createContext<TableContextType>({
+  data: initialData,
+  setData: () => {
+    initialData;
+  },
+});
 
 type ContextProviderProps = {
   children: ReactNode;
 };
 
 export const TableContextProvider = ({ children }: ContextProviderProps) => {
-  const [data, setData] = useState<ConsultantDataType[]>(consultantsData);
+  const [data, setData] = useState<ConsultantCalendarType>(initialData);
   const value = { data, setData };
 
-  return <TableContext.Provider value={value}>{children}</TableContext.Provider>
-  ;
+  return (
+    <TableContext.Provider value={value}>{children}</TableContext.Provider>
+  );
 };
 export const useTableContext = () => useContext(TableContext);
