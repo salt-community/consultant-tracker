02575607f122a921f59df9d51523e17e181dacd2
JagodaBodnar <jagodabodnar90@gmail.com>
2024-08-05 10:26:21 +0200

    fetch data from backend and implement in ganttchart

--- frontend/src/components/dashboard/dashboard.tsx
+++ frontend/src/components/dashboard/dashboard.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Infographic from "./infographic/infographic";
 import "./dashboard.css";
-import { infographicData } from "@/mockData";
+import {consultantItems, infographicData} from "@/mockData";
 import FilterField from "../filter/filter";
 import EnhancedTable from "../table/table";
 import ViewSwitch from "@/components/view-switch/view-switch";
@@ -9,16 +9,66 @@ import GanttChart from "@/components/gantt-chart/gantt-chart";
 import { useState, useEffect } from "react";
 import { getDashboardData } from "@/api";
 import { useTableContext } from "@/context/table";
+import dayjs from "dayjs";
+import {ConsultantItemsType} from "@/types";
 
 const Dashboard = () => {
   const [view, setView] = useState<string>("table");
   const tableData = useTableContext();
+  const [items, setItems] = useState<ConsultantItemsType[]>([]);
+  const [groups, setGroups] = useState([])
 
   useEffect(() => {
     getDashboardData().then((data) => {
       tableData.setData(data), tableData.setFilteredData(data);
     });
+    fetch("http://localhost:8080/api/consultants")
+      .then(res=> res.json())
+      .then(res => {
+        const data = res.consultants.map(el => {
+          return el.registeredTimeDtoList.map(item => {
+            return {
+              id: 1,
+              group: 1,
+              start_time: dayjs("2024-07-22T00:00:00"),
+              end_time: dayjs("2024-08-22T23:59:59"),
+              itemProps: {
+                style: {
+                  zIndex: 5,
+                  background: selectColor(item.type),
+                  outline: "none",
+                  border: "none"
+                },
+              },
+            }
+          })
+        })
+        console.log(data)
+        setItems(data);
+        return res;
+      })
+      .then(res => {
+        const data = res.consultants.map(el => {
+          return {id: 1, title: el.fullName}
+        })
+        console.log('groups', data)
+        setGroups(data);
+      })
   }, []);
+  const selectColor = (type: string) => {
+    switch (type) {
+      case "Konsult-tid":
+        return "#29B269";
+      case "Semester":
+        return "yellow";
+      case "Sjuk":
+        return "green";
+      case "Remaining Days":
+        return "gray";
+      default:
+        return "red"
+    }
+  }
 
   return (
     <>
@@ -37,7 +87,7 @@ const Dashboard = () => {
       </div>
       <FilterField />
       <ViewSwitch setView={setView} view={view} />
-      {view === "timeline" && <GanttChart />}
+      {view === "timeline" && <GanttChart items={items} groups={groups}/>}
       {view === "table" && <EnhancedTable />}
     </>
   );
--- frontend/src/components/gantt-chart/gantt-chart.tsx
+++ frontend/src/components/gantt-chart/gantt-chart.tsx
@@ -1,22 +1,29 @@
 "use client"
-import { useClientsContext } from "@/context/clients";
+import {useClientsContext} from "@/context/clients";
 import SingleConsultant from "@/components/gantt-chart/single-consultant/single-consultant";
-import { usePathname } from "next/navigation";
+import {usePathname} from "next/navigation";
 import Timeline from "react-calendar-timeline";
 import "react-calendar-timeline/lib/Timeline.css";
 import moment from "moment";
-import { consultantsCalendar, consultantItems } from "@/mockData";
-import { useEffect, useState } from "react";
-import { ConsultantItemsType } from "@/types";
-import { getDatesForRemainingTime } from "@/helperMethods";
+import {consultantsCalendar, consultantItems} from "@/mockData";
+import {useEffect, useState} from "react";
+import {ConsultantItemsType, ConsultantsCalendarType} from "@/types";
+import {getDatesForRemainingTime} from "@/helperMethods";
 import "./gantt-chart.css";
-
-const GanttChart = () => {
+import {getDashboardData} from "@/api";
+import dayjs from "dayjs";
+type Props={
+  items: ConsultantItemsType[],
+  groups: ConsultantsCalendarType[]
+}
+const GanttChart = ({items, groups}:Props) => {
+  console.log('ganttchart items', items)
+  console.log('ganttchart groups', groups)
   const client = useClientsContext();
   const idParam = usePathname().split("/").pop();
   const singleClient = client.data.filter((el) => el.id === idParam)[0];
-  const [items, setItems] = useState<ConsultantItemsType[]>(consultantItems);
-
+  // const [items, setItems] = useState<ConsultantItemsType[]>(consultantItems);
+  // const [groups, setGroups] = useState([])
   // useEffect(() => {
   //   const remainingTimeItem = getDatesForRemainingTime(1, items);
   //   const newItem = {
@@ -35,18 +42,54 @@ const GanttChart = () => {
   //   setItems(pushedItem);
   // }, []);
 
- console.log(singleClient);
+  //
+  // useEffect(() => {
+  //   fetch("http://localhost:8080/api/consultants")
+  //     .then(res=> res.json())
+  //     .then(res => {
+  //       const data = res.consultants.map(el => {
+  //         return el.registeredTimeDtoList.map(item => {
+  //           return {
+  //             id: item.registeredTimeId,
+  //             group: el.id,
+  //             start_time: dayjs(item.startDate),
+  //             end_time: dayjs(item.endDate),
+  //             // style: {
+  //             //   zIndex: 1,
+  //             //   background: selectColor(item.type),
+  //             //   outline: "none",
+  //             //   border: "none"
+  //             // },
+  //           }
+  //         })
+  //       })
+  //       setItems(data);
+  //       return res;
+  //     })
+  //     .then(res => {
+  //       const data = res.consultants.map(el => {
+  //         return {id: el.id, title: el.fullName}
+  //       })
+  //       setGroups(data);
+  //     })
+  // }, []);
+  // const handleTimeChange = (visibleTimeStart, visibleTimeEnd, updateScrollCanvas) => {
+  //   updateScrollCanvas(moment(this.state.defaultTimeStart).valueOf(),
+  //     moment(this.state.defaultTimeEnd).valueOf());
+  // }
 
   return (
     <div>
-      <div>
-        <Timeline
-          groups={consultantsCalendar}
-          items={items}
-          defaultTimeStart={moment().add(-17, "day")}
-          defaultTimeEnd={moment().add(4, "day")}
-        />
-      </div>
+        <div>
+            <Timeline
+                groups={groups}
+                items={items}
+                // buffer={5}
+                // onTimeChange={handleTimeChange}
+                defaultTimeStart={moment().add(-17, "day")}
+                defaultTimeEnd={moment().add(4, "day")}
+            />
+        </div>
       {/*{singleClient.listOfConsultants.map((consultant) => {*/}
       {/*  const { name, id } = consultant;*/}
       {/*  return <SingleConsultant name={name} id={id} key={id} />;*/}
