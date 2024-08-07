f1378e39576fd02e188f6b3fb94e6c04093a43cd
kokakarova <kostadinkakarova@gmail.com>
2024-08-07 15:16:53 +0200

    remove dagsmart client

--- backend/src/main/java/com/example/backend/client/dagsmart/dto/Name.java
+++
@@ -1,4 +0,0 @@
-package com.example.backend.client.dagsmart.dto;
-
-public record Name(String en) {
-}
--- backend/src/main/java/com/example/backend/client/dagsmart/dto/RedDaysFromDagsmartDto.java
+++
@@ -1,9 +0,0 @@
-package com.example.backend.client.dagsmart.dto;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public record RedDaysFromDagsmartDto(String date,
-                                     String code,
-                                     @JsonProperty("name")
-                                     Name name) {
-}
