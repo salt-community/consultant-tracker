486197ef3e96d23d070176023a86a05801f97d32
JagodaBodnar <jagodabodnar90@gmail.com>
2024-08-09 20:28:28 +0200

    BE- add test for countTotalWorkedHours method

--- backend/src/test/java/com/example/backend/registeredTime/RegisteredTimeServiceTest.java
+++ backend/src/test/java/com/example/backend/registeredTime/RegisteredTimeServiceTest.java
@@ -6,6 +6,8 @@ import com.example.backend.consultant.ConsultantService;
 import com.example.backend.redDays.RedDaysService;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -13,8 +15,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
+import static com.example.backend.client.timekeeper.Activity.CONSULTANCY_TIME;
+import static com.example.backend.client.timekeeper.Activity.OWN_ADMINISTRATION;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -50,6 +55,18 @@ class RegisteredTimeServiceTest {
         assertEquals(2, actualResult.size());
     }
 
-
+    @Test
+    public void shouldReturnTotalWorkedHours() {
+        Mockito.lenient().when(registeredTimeRepository.getSumOfTotalHoursByConsultantIdAndProjectName(
+                        UUID.fromString("68c670d6-3038-4fca-95be-2669aaf0b549"),
+                        CONSULTANCY_TIME.activity))
+                .thenReturn(Optional.of(150D));
+        Mockito.lenient().when(registeredTimeRepository.getSumOfTotalHoursByConsultantIdAndProjectName(
+                        UUID.fromString("68c670d6-3038-4fca-95be-2669aaf0b549"),
+                        OWN_ADMINISTRATION.activity))
+                .thenReturn(Optional.of(100D));
+        double actualResult = registeredTimeService.countTotalWorkedHours(UUID.fromString("68c670d6-3038-4fca-95be-2669aaf0b549"));
+        assertEquals(250D, actualResult);
+    }
 
 }
