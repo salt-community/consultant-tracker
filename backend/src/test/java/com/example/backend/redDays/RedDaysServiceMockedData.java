c78b870316b65444aadcdb7b788a414d1cbdb0d3
JagodaBodnar <jagodabodnar90@gmail.com>
2024-08-09 11:50:28 +0200

    BE- refactor RedDaysServiceTest by extracting mockedData to separate class.

---
+++ backend/src/test/java/com/example/backend/redDays/RedDaysServiceMockedData.java
@@ -0,0 +1,36 @@
+package com.example.backend.redDays;
+
+import org.assertj.core.util.Lists;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+public class RedDaysServiceMockedData {
+    public static List<RedDays> createMockedRedDaysDataNO(){
+        RedDays mockedRedDay = new RedDays(
+                UUID.fromString("2e6d51fe-713a-413b-8769-0180aa60c084"),
+                LocalDate.parse("2024-01-01"),
+                "Red day test",
+                "NO"
+        );
+        RedDays mockedRedDay2 = new RedDays(
+                UUID.fromString("2e6d51fe-713a-413b-8769-0180aa60c084"),
+                LocalDate.parse("2024-12-24"),
+                "Red day test2",
+                "NO"
+        );
+        return Lists.newArrayList(mockedRedDay, mockedRedDay2);
+    }
+    public static List<RedDays> createMockedRedDaysSE(){
+        RedDays mockedRedDay = new RedDays(
+                UUID.fromString("2e6d51fe-713a-413b-8769-0180aa60c084"),
+                LocalDate.parse("2024-01-01"),
+                "Red day test",
+                "SE"
+        );
+        return Lists.newArrayList(mockedRedDay);
+    }
+
+
+}
--- backend/src/test/java/com/example/backend/redDays/RedDaysServiceTest.java
+++ backend/src/test/java/com/example/backend/redDays/RedDaysServiceTest.java
@@ -38,14 +38,7 @@ class RedDaysServiceTest {
 
     @Test
     public void shouldReturnOneAnd1JanuaryWhenCountrySE() {
-        var mockedRedDay = new RedDays(
-                UUID.fromString("2e6d51fe-713a-413b-8769-0180aa60c084"),
-                LocalDate.parse("2024-01-01"),
-                "Red day test",
-                "SE"
-        );
-        List<RedDays> mockedList = Lists.newArrayList(mockedRedDay);
-        Mockito.when(redDaysRepository.findAllByCountry("SE")).thenReturn(mockedList);
+        Mockito.when(redDaysRepository.findAllByCountry("SE")).thenReturn(RedDaysServiceMockedData.createMockedRedDaysSE());
         List<LocalDate> actualResult = redDaysService.getRedDays("SE");
         assertEquals(actualResult.size(), 1);
         assertEquals(actualResult.getFirst(), LocalDate.parse("2024-01-01"));
@@ -53,21 +46,8 @@ class RedDaysServiceTest {
 
     @Test
     public void shouldReturnTwoAnd24DecemberWhenCountryNO() {
-        var mockedRedDay = new RedDays(
-                UUID.fromString("2e6d51fe-713a-413b-8769-0180aa60c084"),
-                LocalDate.parse("2024-01-01"),
-                "Red day test",
-                "NO"
-        );
-        var mockedRedDay2 = new RedDays(
-                UUID.fromString("2e6d51fe-713a-413b-8769-0180aa60c084"),
-                LocalDate.parse("2024-12-24"),
-                "Red day test2",
-                "NO"
-        );
-        var expectedResult = Lists.newArrayList(LocalDate.parse("2024-01-01"), LocalDate.parse("2024-12-24"));
-        List<RedDays> mockedList = Lists.newArrayList(mockedRedDay, mockedRedDay2);
-        Mockito.when(redDaysRepository.findAllByCountry("NO")).thenReturn(mockedList);
+        List<LocalDate> expectedResult = Lists.newArrayList(LocalDate.parse("2024-01-01"), LocalDate.parse("2024-12-24"));
+        Mockito.when(redDaysRepository.findAllByCountry("NO")).thenReturn(RedDaysServiceMockedData.createMockedRedDaysDataNO());
         List<LocalDate> actualResult = redDaysService.getRedDays("NO");
         assertEquals(actualResult.size(), 2);
         assertEquals(actualResult.get(1), expectedResult.get(1));
@@ -75,18 +55,10 @@ class RedDaysServiceTest {
 
     @Test
     public void shouldReturnFalseFor2JanuaryWhenCountrySE() {
-        var mockedRedDay = new RedDays(
-                UUID.fromString("2e6d51fe-713a-413b-8769-0180aa60c084"),
-                LocalDate.parse("2024-01-01"),
-                "Red day test",
-                "SE"
-        );
-        List<RedDays> mockedList = Lists.newArrayList(mockedRedDay);
         Mockito.lenient().when(consultantService.getCountryCodeByConsultantId(
                         UUID.fromString("45ec353f-b0f5-4a51-867e-8d0d84d11573")))
                 .thenReturn("SE");
-        Mockito.lenient().when(redDaysRepository.findAllByCountry("SE")).thenReturn(mockedList);
-
+        Mockito.lenient().when(redDaysRepository.findAllByCountry("SE")).thenReturn(RedDaysServiceMockedData.createMockedRedDaysSE());
         boolean actualResult = redDaysService.isRedDay(
                 LocalDate.parse("2024-01-02"),
                 UUID.fromString("45ec353f-b0f5-4a51-867e-8d0d84d11573"));
