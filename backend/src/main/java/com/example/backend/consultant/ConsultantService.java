79236709ab340e708dcd362f33b64f98deacfa0a
kokakarova <kostadinkakarova@gmail.com>
2024-08-12 10:36:23 +0200

    refactor ConsultantService

--- backend/src/main/java/com/example/backend/consultant/ConsultantService.java
+++ backend/src/main/java/com/example/backend/consultant/ConsultantService.java
@@ -2,35 +2,39 @@ package com.example.backend.consultant;
 
 import com.example.backend.client.timekeeper.TimekeeperClient;
 import com.example.backend.client.timekeeper.dto.TimekeeperUserDto;
-import com.example.backend.consultant.dto.ConsultantResponseDto;
 import com.example.backend.consultant.dto.ConsultantResponseListDto;
 import com.example.backend.registeredTime.RegisteredTimeService;
 import com.example.backend.tag.Tag;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
 
 
 @Service
+@Data
 @RequiredArgsConstructor
 public class ConsultantService {
-    private final ConsultantRepository consultantRepository;
-    private final TimekeeperClient timekeeperClient;
-    private final RegisteredTimeService registeredTimeService;
+    private ConsultantRepository consultantRepository;
+    private TimekeeperClient timekeeperClient;
+    private RegisteredTimeService registeredTimeService;
+
 
     public ConsultantResponseListDto getAllConsultantDtos(int page, int pageSize, String name, String pt, String client) {
         Page<Consultant> consultantsList = getAllConsultantsPageable(page, pageSize, name, pt, client);
-        List<ConsultantResponseDto> consultantsDto = consultantsList.stream()
-                .map(registeredTimeService::getConsultantTimelineItems).toList();
+
         return new ConsultantResponseListDto(
                 page,
                 consultantsList.getTotalPages(),
                 consultantsList.getTotalElements(),
-                consultantsDto);
+                new ArrayList<>(consultantsList.stream()
+                        .map(registeredTimeService::getConsultantTimelineItems).toList()));
     }
 
     public Page<Consultant> getAllConsultantsPageable(int page, int pageSize, String name, String pt, String client) {
@@ -42,6 +46,10 @@ public class ConsultantService {
         return consultantRepository.findAll();
     }
 
+    public List<Consultant> getAllActiveConsultants() {
+        return consultantRepository.findAllByActiveTrue();
+    }
+
     //    @Scheduled(cron = "0 0 0 * * *")
     public void fetchDataFromTimekeeper() {
         List<TimekeeperUserDto> timekeeperUserDto = timekeeperClient.getUsers();
@@ -50,6 +58,7 @@ public class ConsultantService {
         registeredTimeService.fetchAndSaveTimeRegisteredByConsultant();
         fillClientAndResponsiblePt();
     }
+// Test in integration tests
 
     private void updateConsultantTable(List<TimekeeperUserDto> timekeeperUserDto) {
         timekeeperUserDto.forEach(tkUser -> {
@@ -71,10 +80,7 @@ public class ConsultantService {
         });
     }
 
-    public List<Consultant> getAllActiveConsultants() {
-        return consultantRepository.findAllByActiveTrue();
-    }
-
+    // Test in integration tests
     private void updateIsActiveForExistingConsultant(TimekeeperUserDto tkUser) {
         List<Consultant> consultants = getAllConsultants();
         consultants.stream()
