17ad1f7e904546a738288a939fe47c7a278cd4cf
kokakarova <kostadinkakarova@gmail.com>
2024-08-15 11:52:56 +0200

    fix filter for pts and clients and encode requestParam string

--- frontend/src/components/dashboard/dashboard.tsx
+++ frontend/src/components/dashboard/dashboard.tsx
@@ -1,27 +1,33 @@
 "use client";
 import Infographic from "./infographic/infographic";
 import "./dashboard.css";
-import {infographicData} from "@/mockData";
+import { infographicData } from "@/mockData";
 import FilterField from "../filter/filter";
 import EnhancedTable from "../table/table";
 import ViewSwitch from "@/components/view-switch/view-switch";
 import GanttChart from "@/components/gantt-chart/gantt-chart";
-import React, {useState, useEffect} from "react";
-import {getDashboardData} from "@/api";
-import {useTableContext} from "@/context/table";
+import React, { useState, useEffect } from "react";
+import { getDashboardData } from "@/api";
+import { useTableContext } from "@/context/table";
 import dayjs from "dayjs";
-import {selectColor} from "@/utils/utils";
-import {AllClientsAndResponsiblePtResponse, ConsultantFetchType, RegisteredTimeItemType} from "@/types";
+import { selectColor } from "@/utils/utils";
+import {
+  AllClientsAndResponsiblePtResponse,
+  ConsultantFetchType,
+  RegisteredTimeItemType,
+} from "@/types";
+import { url } from "inspector";
 
 const Dashboard = () => {
   const [view, setView] = useState<string>("timeline");
   const tableData = useTableContext();
   const [items, setItems] = useState<any[]>([]);
-  const [groups, setGroups] = useState([])
+  const [groups, setGroups] = useState([]);
   const [listOfResponsiblePt, setListOfResponsiblePts] = useState<string[]>([]);
   const [listOfClients, setListOfClients] = useState<string[]>([]);
   const [totalItems, setTotalItems] = useState<number>(0);
-  const [filterPts, setFilterPts]= useState({"pt":["Josefin Stål"]});
+  const [filterPts, setFilterPts] = useState(["Josefin Stål"]);
+  const [filterClients, setFilterClients] = useState<string[]>([]);
 
   useEffect(() => {
     getDashboardData().then((data) => {
@@ -30,67 +36,83 @@ const Dashboard = () => {
     });
 
     fetch("http://localhost:8080/api/consultants/getAllClientsAndPts")
-      .then(res => res.json())
+      .then((res) => res.json())
       .then((res: AllClientsAndResponsiblePtResponse) => {
-        setListOfResponsiblePts(res.pts)
-        setListOfClients(res.clients)
-      })
+        setListOfResponsiblePts(res.pts);
+        setListOfClients(res.clients);
+      });
+    const filterPtsEncodeUriString = filterPts
+      .map((p) => `pt=${encodeURIComponent(p)}`)
+      .join("&");
 
-    fetch(`http://localhost:8080/api/consultants?page=3&pt=${filterPts}`)
-      .then(res => res.json())
-      .then(res => {
+    const filterClientsEncodeUriString = filterClients
+      .map((c) => `client=${encodeURIComponent(c)}`)
+      .join("&")
+      .replaceAll("-", "%2D")
+      .replaceAll(".", "%2E");
+
+
+    fetch(
+      `http://localhost:8080/api/consultants?page=0&${filterPtsEncodeUriString}&${filterClientsEncodeUriString}`
+    )
+      .then((res) => res.json())
+      .then((res) => {
         const data = res.consultants.flatMap((el: ConsultantFetchType) => {
-          return el.registeredTimeDtoList.map((item: RegisteredTimeItemType) => {
-            return {
-              id: item.registeredTimeId,
-              group: el.id,
-              title: item.type,
-              details: {
-                name: el.fullName,
-                totalRemainingDays: el.totalDaysStatistics.totalRemainingDays,
-                totalWorkedDays: el.totalDaysStatistics.totalWorkedDays,
-                totalVacationDaysUsed: el.totalDaysStatistics.totalVacationDaysUsed,
-                totalDays: item.days,
-                totalWorkedHours: el.totalDaysStatistics.totalWorkedHours,
-                totalRemainingHours: el.totalDaysStatistics.totalRemainingHours,
-                client: el.client,
-                projectName: item.projectName
-              },
-              start_time: dayjs(item.startDate),
-              end_time: dayjs(item.endDate),
-              className: "",
-              itemProps: {
-                style: {
-                  zIndex: 1,
-                  background: selectColor(item.type),
-                  outline: "none",
-                  borderColor: selectColor(item.type),
-                  borderRightWidth: "0"
+          return el.registeredTimeDtoList.map(
+            (item: RegisteredTimeItemType) => {
+              return {
+                id: item.registeredTimeId,
+                group: el.id,
+                title: item.type,
+                details: {
+                  name: el.fullName,
+                  totalRemainingDays: el.totalDaysStatistics.totalRemainingDays,
+                  totalWorkedDays: el.totalDaysStatistics.totalWorkedDays,
+                  totalVacationDaysUsed:
+                    el.totalDaysStatistics.totalVacationDaysUsed,
+                  totalDays: item.days,
+                  totalWorkedHours: el.totalDaysStatistics.totalWorkedHours,
+                  totalRemainingHours:
+                    el.totalDaysStatistics.totalRemainingHours,
+                  client: el.client,
+                  projectName: item.projectName,
                 },
-              },
+                start_time: dayjs(item.startDate),
+                end_time: dayjs(item.endDate),
+                className: "",
+                itemProps: {
+                  style: {
+                    zIndex: 1,
+                    background: selectColor(item.type),
+                    outline: "none",
+                    borderColor: selectColor(item.type),
+                    borderRightWidth: "0",
+                  },
+                },
+              };
             }
-          })
-        })
+          );
+        });
         setItems(data);
         return res;
       })
-      .then(res=>{
-        setTotalItems(res.totalConsultants)
+      .then((res) => {
+        setTotalItems(res.totalConsultants);
         return res;
       })
-      .then(res => {
+      .then((res) => {
         const data = res.consultants.map((el: ConsultantFetchType) => {
-          return {id: el.id, title: el.fullName}
-        })
+          return { id: el.id, title: el.fullName };
+        });
         setGroups(data);
-      })
-  }, []);
+      });
+  }, [filterPts, filterClients]);
 
   return (
     <>
       <div className="dashboard-infographic__card">
         {infographicData.map((element, index) => {
-          const {title, amount, variant} = element;
+          const { title, amount, variant } = element;
           return (
             <Infographic
               key={index}
@@ -101,15 +123,23 @@ const Dashboard = () => {
           );
         })}
       </div>
-      <FilterField lisOfResponsiblePt={listOfResponsiblePt}
-                   setListOfResponsiblePt={setListOfResponsiblePts}
-                   setListOfClients={setListOfClients}
-                   listOfClients={listOfClients}
-
+      <FilterField
+        lisOfResponsiblePt={listOfResponsiblePt}
+        listOfClients={listOfClients}
+        filterPts={filterPts}
+        filterClients={filterClients}
+        setFilterPts={setFilterPts}
+        setFilterClients={setFilterClients}
       />
-      <ViewSwitch setView={setView} view={view}/>
-      {view === "timeline" && <GanttChart itemsProps={items} groupsProps={groups} totalItems={totalItems}/>}
-      {view === "table" && <EnhancedTable totalItems={totalItems}/>}
+      <ViewSwitch setView={setView} view={view} />
+      {view === "timeline" && (
+        <GanttChart
+          itemsProps={items}
+          groupsProps={groups}
+          totalItems={totalItems}
+        />
+      )}
+      {view === "table" && <EnhancedTable totalItems={totalItems} />}
     </>
   );
 };
--- frontend/src/components/filter/filter.tsx
+++ frontend/src/components/filter/filter.tsx
@@ -1,63 +1,51 @@
 "use client";
-import {ChangeEvent, Dispatch, SetStateAction, useEffect, useState} from "react";
+import {
+  ChangeEvent,
+  Dispatch,
+  SetStateAction,
+  useEffect,
+  useState,
+} from "react";
 import "./filter.css";
 import { useTableContext } from "@/context/table";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import Multiselect from "@/components/filter/multiselect/multiselect";
-type Props ={
-  lisOfResponsiblePt: string[],
-  listOfClients: string[],
-  setListOfResponsiblePt: Dispatch<SetStateAction<string[]>>
-  setListOfClients: Dispatch<SetStateAction<string[]>>
-}
-function FilterField({lisOfResponsiblePt, listOfClients, setListOfResponsiblePt, setListOfClients}:Props) {
+type Props = {
+  lisOfResponsiblePt: string[];
+  listOfClients: string[];
+  filterPts: string[];
+  filterClients: string[];
+  setFilterPts: Dispatch<SetStateAction<string[]>>;
+  setFilterClients: Dispatch<SetStateAction<string[]>>;
+};
+function FilterField({
+  lisOfResponsiblePt,
+  listOfClients,
+  filterPts,
+  filterClients,
+  setFilterPts,
+  setFilterClients,
+}: Props) {
   const [filterValue, setFilterValue] = useState("");
-  const [responsiblePts, setResponsiblePts] = useState(["Josefin Stål"]);
-  const [clients, setClients] = useState([]);
-  const data = useTableContext();
-  // const filterData = () => {
-
-  //   const filteredData = [...data.data.consultants].filter((df) => {
-  //     const name = df.fullName.toLowerCase();
-  //     const searchValue = filterValue.toLowerCase();
-  //     if (responsiblePts.length === 0) {
-  //       return name.includes(searchValue);
-  //     }
-  //     return name.includes(searchValue) && responsiblePts.includes("Josefin Stål");
-  //   });
-  //   data!.setFilteredData({
-  //     pageNumber: 0,
-  //     totalPages: 0,
-  //     totalConsultants: 0,
-  //     consultants: filteredData,
-  //   });
-  // };
 
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     setFilterValue(e.target.value);
-    // filterData();
   };
 
   const handleClear = () => {
     setFilterValue("");
-    setResponsiblePts(["Josefin Stål"]);
-    setClients([])
+    setFilterPts([]);
+    setFilterClients([]);
   };
 
   const handlePtsSelection = (selectionArr: string[]) => {
-    setResponsiblePts(selectionArr);
-    // filterData();
+    setFilterPts(selectionArr);
   };
 
   const handleClientSelection = (selectionArr: string[]) => {
-    // setListOfClients(selectionArr);
-    // filterData();
+    setFilterClients(selectionArr);
   };
-
-  useEffect(() => {
-    // filterData();
-  }, [responsiblePts, filterValue]);
 
   return (
     <section className="filter-section">
@@ -76,15 +64,15 @@ function FilterField({lisOfResponsiblePt, listOfClients, setListOfResponsiblePt,
         <Multiselect
           fullList={listOfClients}
           handleSelection={handleClientSelection}
-          setSelection={setClients}
-          selected={clients}
+          setSelection={setFilterClients}
+          selected={filterClients}
           label="Filter by client"
         />
         <Multiselect
           fullList={lisOfResponsiblePt}
           handleSelection={handlePtsSelection}
-          setSelection={setResponsiblePts}
-          selected={responsiblePts}
+          setSelection={setFilterPts}
+          selected={filterPts}
           label="Filter by responsible pt"
         />
         <Button onClick={handleClear} variant="contained">
