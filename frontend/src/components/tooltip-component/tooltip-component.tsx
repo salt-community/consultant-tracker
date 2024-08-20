22670819befbecb1ea3d6d6c55b0e32621e5fbca
Anthony33720 <raynaud.anthony@gmail.com>
2024-08-20 16:00:47 +0300

    try to fix problem

--- frontend/src/components/tooltip-component/tooltip-component.tsx
+++ frontend/src/components/tooltip-component/tooltip-component.tsx
@@ -2,7 +2,9 @@ import Tooltip from "@mui/material/Tooltip";
 import { Dispatch, SetStateAction } from "react";
 import { ConsultantItemsType } from "@/types";
 import "./tooltip-component.css";
-
+import { Weekend } from "@mui/icons-material";
+import { differenceInBusinessDays, isWeekend } from "date-fns";
+import { Dayjs } from "dayjs";
 type Props = {
   content: ConsultantItemsType;
   setOpenTooltip: Dispatch<SetStateAction<boolean>>;
@@ -10,12 +12,33 @@ type Props = {
 };
 
 const TooltipComponent = ({ setOpenTooltip, openTooltip, content }: Props) => {
-  console.log("Selected Item Content:", content);
-  console.log("Tooltip Open:", openTooltip);
-
   const handleTooltipClose = () => {
     setOpenTooltip(false);
   };
+
+  // const startDate = new Date(content.start_time);
+  // const endDate = new Date(content.end_time);
+
+  // let totalDays = differenceInBusinessDays(startDate, endDate);
+
+  // if (!isWeekend(endDate)) {
+  //   totalDays = +1;
+  // }
+
+  // const week = (start_date: Dayjs, end_date: Dayjs) => {
+  //   let totalDays = 0;
+  //   let currentDate = start_date.startOf("day");
+  //   while (currentDate < end_date) {
+  //     const isWeekend = currentDate.day() === 0 || currentDate.day() === 6;
+  //     if (!isWeekend) {
+  //       totalDays = +1;
+  //     }
+  //     currentDate.add(1, "day");
+  //   }
+  //   return totalDays;
+  // };
+
+  // let businessDays = week(content.start_time, content.end_time);
 
   return (
     <Tooltip
@@ -37,10 +60,7 @@ const TooltipComponent = ({ setOpenTooltip, openTooltip, content }: Props) => {
         <div className="item-details">
           <h3>{content.details.name}</h3>
           <p>{content.title}</p>
-          <p>
-            Total Days Selected:{" "}
-            {content.end_time.diff(content.start_time, "day") + 1}
-          </p>
+          {/* <p>Total Days Selected: {businessDays}</p> */}
           <p>Start Date: {content.start_time.format("ddd, DD-MMM-YY")}</p>
           <p>End Date: {content.end_time.format("ddd, DD-MMM-YY")}</p>
         </div>
