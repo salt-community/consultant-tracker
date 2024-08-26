3697b669c97e9dbfbc6d5c0aead56a18236c7a04
kokakarova <kostadinkakarova@gmail.com>
2024-08-26 13:34:04 +0200

    create ENUM for projectName saved in DB that are not clients (PGP, upskilling)

--- backend/src/main/java/com/example/backend/consultant/NotClient.java
+++ backend/src/main/java/com/example/backend/consultant/NotClient.java
@@ -1,4 +1,10 @@
 package com.example.backend.consultant;
 
 public enum NotClient {
+    PGP("PGP"),
+    UPSKILLING("Upskilling");
+    public final String value;
+    NotClient(String value) {
+        this.value = value;
+    }
 }
