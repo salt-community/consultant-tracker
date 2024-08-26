296bfa9317bfcd69c19cc772bedce1cd1044fef8
kokakarova <kostadinkakarova@gmail.com>
2024-08-26 13:34:27 +0200

    add methods and tests to fetch clients info from DB

--- backend/src/main/java/com/example/backend/registeredTime/RegisteredTimeRepository.java
+++ backend/src/main/java/com/example/backend/registeredTime/RegisteredTimeRepository.java
@@ -1,9 +1,12 @@
 package com.example.backend.registeredTime;
 
+import org.springframework.cglib.core.Local;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -16,4 +19,10 @@ public interface RegisteredTimeRepository extends JpaRepository<RegisteredTime, 
      @Query("SELECT SUM(t.totalHours) FROM RegisteredTime t WHERE t.id.consultantId = (:id) AND t.type LIKE %:type%")
      Optional<Double> getSumOfTotalHoursByConsultantIdAndType(UUID id, String type);
      RegisteredTime findFirstById_ConsultantIdAndTypeIsOrderByEndDateDesc(UUID id, String type);
+
+     @Query("SELECT DISTINCT t.projectName FROM RegisteredTime t WHERE t.id.consultantId = (:consultantId)")
+     List<String> findDistinctProjectNameBydId_ConsultantId(@Param("consultantId")UUID consultantId);
+
+     RegisteredTime findFirstByProjectNameAndId_ConsultantIdOrderById_StartDateAsc(String projectName, UUID consultantId);
+     RegisteredTime findFirstByProjectNameAndId_ConsultantIdOrderByEndDateDesc(String projectName, UUID consultantId);
 }
