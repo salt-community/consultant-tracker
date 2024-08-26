4c85a95f35d5b12ad65bf52ddb565c4a477f1b4c
kokakarova <kostadinkakarova@gmail.com>
2024-08-26 13:36:26 +0200

    create new DTO for consultantById response

--- backend/src/main/java/com/example/backend/consultant/dto/SingleConsultantResponseListDto.java
+++ backend/src/main/java/com/example/backend/consultant/dto/SingleConsultantResponseListDto.java
@@ -1,4 +1,37 @@
 package com.example.backend.consultant.dto;
 
-public record SingleConsultantResponseListDto() {
+import com.example.backend.consultant.Consultant;
+import com.example.backend.redDay.CountryCode;
+
+import java.util.List;
+import java.util.UUID;
+
+import static com.example.backend.utils.Country.SWEDEN;
+
+public record SingleConsultantResponseListDto(UUID id,
+                                              String fullName,
+                                              String email,
+                                              String phoneNumber,
+                                              String responsiblePt,
+                                              String client,
+                                              String country,
+                                              TotalDaysStatisticsDto totalDaysStatistics,
+                                              List<ClientsList> clientsList) {
+
+    public static SingleConsultantResponseListDto toDto(
+            Consultant consultant,
+            TotalDaysStatisticsDto totalDaysStatistics,
+            List<ClientsList> clientsList) {
+        return new SingleConsultantResponseListDto(
+                consultant.getId(),
+                consultant.getFullName(),
+                consultant.getEmail(),
+                consultant.getPhoneNumber(),
+                consultant.getResponsiblePT(),
+                consultant.getClient(),
+                consultant.getCountry().equals(SWEDEN.country) ? CountryCode.SE.countryCode : CountryCode.NO.countryCode,
+                totalDaysStatistics,
+                clientsList
+        );
+    }
 }
