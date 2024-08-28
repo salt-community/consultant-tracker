2961704fe771c69f9a713808a92a1a1bcb7b521f
kokakarova <kostadinkakarova@gmail.com>
2024-08-28 17:00:55 +0200

    edit Schedule component to display meetings
    Co-authored-by: Anthony <raynaud.anthony@gmail.com>
    Co-authored-by: Mauro <mauroale2501@gmail.com>
    Co-authored-by: Jagoda <jagodabodnar90@gmail.com>

--- frontend/src/components/consultant-detail/schedule/schedule.tsx
+++ frontend/src/components/consultant-detail/schedule/schedule.tsx
@@ -1,38 +1,54 @@
 "use client";
 
 import { consultantDetailsData } from "@/mockData";
-import { ConsultantDetailsDataType } from "@/types";
+import { ConsultantDetailsDataType, MeetingsType } from "@/types";
 import React, { useEffect, useState } from "react";
 import "./schedules.css";
+import SingleDetailField from "@/components/single-detail-field/single-detail-field";
 
-const Schedule = () => {
-  const idParam = "001";
-  const [scheduleData, setScheduleData] = useState<ConsultantDetailsDataType>(
-    consultantDetailsData[0]
-  );
+type Props = {
+  meetings: MeetingsType[];
+};
 
+const Schedule = ({ meetings }: Props) => {
+  const [scheduleData, setScheduleData] = useState<MeetingsType[]>(meetings);
+  console.log("shceduled data", scheduleData);
   useEffect(() => {
-    const consultantById = consultantDetailsData.filter(
-      (el) => el.id == idParam
-    );
-    setScheduleData(consultantById[0]);
-  }, [idParam]);
+    setScheduleData(meetings);
+  }, [meetings]);
 
-  return (
-    scheduleData && (
-      <div className="meetings-schedule__wrapper">
-        {scheduleData.meetings.map((item) => {
-          const { id, date, title, description } = item;
-          return (
-            <div key={id}>
-              <p>Title: {title}</p>
-              <p>Date : {date}</p>
-              <p>Description: {description}</p>
-            </div>
-          );
-        })}
-      </div>
-    )
+  const formatTitle = (title: string) => {
+    switch (title) {
+      case "FIRST":
+        return "First regular meeting";
+      case "SECOND":
+        return "Second regular meeting";
+      case "THIRD":
+        return "Third regular meeting";
+      case "FOURTH":
+        return "Fourth regular meeting";
+      default:
+        return "Not Scheduled";
+    }
+  };
+
+  return scheduleData.length > 0 ? (
+    <div className="meetings-schedule__wrapper">
+      {scheduleData.map((item, index) => {
+        const { year, weekNumber, title } = item;
+        return (
+          <div key={index} className="meetings-schedule__items">
+            <SingleDetailField title="Title" content={formatTitle(title)} />
+            <SingleDetailField
+              title="Date"
+              content={`${year}, Week: ${weekNumber}`}
+            />
+          </div>
+        );
+      })}
+    </div>
+  ) : (
+    <div> Time has run out </div>
   );
 };
 
