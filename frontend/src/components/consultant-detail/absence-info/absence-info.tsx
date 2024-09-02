d936c0206940678a6cd11837e81ce1f000b2afe2
kokakarova <kostadinkakarova@gmail.com>
2024-09-02 11:26:49 +0200

    FE - update style for tabs in personal data
    Co-authored-by: Anthony <raynaud.anthony@gmail.com>
    Co-authored-by: Mauro <mauroale2501@gmail.com>
    Co-authored-by: Jagoda <jagodabodnar90@gmail.com>

--- frontend/src/components/consultant-detail/absence-info/absence-info.tsx
+++ frontend/src/components/consultant-detail/absence-info/absence-info.tsx
@@ -1,26 +1,38 @@
 import "./absence-info.css";
 import SingleDetailField from "@/components/single-detail-field/single-detail-field";
-import {TotalDaysStatisticsType} from "@/types";
+import { TotalDaysStatisticsType } from "@/types";
 
 type Props = {
   totalDaysStatistics: TotalDaysStatisticsType;
 };
 
-const AbsenceInfo = ({totalDaysStatistics}: Props) => {
+const AbsenceInfo = ({ totalDaysStatistics }: Props) => {
   const {
     totalVacationDaysUsed,
     totalSickDays,
     totalParentalLeaveDays,
     totalVABDays,
-    totalUnpaidVacationDays
+    totalUnpaidVacationDays,
   } = totalDaysStatistics;
   return (
     <div className="absence-info__wrapper">
-      <SingleDetailField title="Total vacation days used" content={`${totalVacationDaysUsed}`}/>
-      <SingleDetailField title="Total sick days used" content={`${totalSickDays}`}/>
-      <SingleDetailField title="Total parental leave days used" content={`${totalParentalLeaveDays}`}/>
-      <SingleDetailField title="Total VAB days used" content={`${totalVABDays}`}/>
-      <SingleDetailField title="Total unpaid leave days used" content={`${totalUnpaidVacationDays}`}/>
+      <h3>Total days used:</h3>
+      <div className="absence-info__content">
+        <SingleDetailField
+          title="Vacation"
+          content={`${totalVacationDaysUsed}`}
+        />
+        <SingleDetailField title="Sick" content={`${totalSickDays}`} />
+        <SingleDetailField
+          title="Parental leave"
+          content={`${totalParentalLeaveDays}`}
+        />
+        <SingleDetailField title="VAB" content={`${totalVABDays}`} />
+        <SingleDetailField
+          title="Unpaid leave"
+          content={`${totalUnpaidVacationDays}`}
+        />
+      </div>
     </div>
   );
 };
--- frontend/src/components/single-detail-field/single-detail-field.tsx
+++ frontend/src/components/single-detail-field/single-detail-field.tsx
@@ -1,13 +1,20 @@
-import "./single-detail-field.css"
-type Props ={
-  title: string,
-  content: string
-}
-const SingleDetailField = ({title, content}:Props) => {
+import "./single-detail-field.css";
+type Props = {
+  title: string;
+  content: string;
+};
+const SingleDetailField = ({ title, content }: Props) => {
+  let content2 = "";
+  if (content.includes("@")) {
+    const splitContent = content.split("@");
+    content = splitContent[0];
+    content2 = splitContent[1];
+  }
   return (
     <div className="details__wrapper">
       <p className="details__title">{title}:</p>
       <p>{content}</p>
+      {content2.length > 0 && <p>{`@${content2}`}</p>}
     </div>
   );
 };
