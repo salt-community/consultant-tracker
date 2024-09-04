5c03e393bdecdf35af06ed53ebe3a4a9a2f7b3ea
kokakarova <kostadinkakarova@gmail.com>
2024-09-04 15:29:21 +0200

    BE - filter notion results to map only ones that have title in name and remove unused code
    Co-authored-by: Anthony <raynaud.anthony@gmail.com>
    Co-authored-by: Mauro <mauroale2501@gmail.com>
    Co-authored-by: Jagoda <jagodabodnar90@gmail.com>

--- backend/src/main/java/com/example/backend/client/notion/NotionResponse.java
+++ backend/src/main/java/com/example/backend/client/notion/NotionResponse.java
@@ -8,20 +8,20 @@ import java.util.List;
 import java.util.Map;
 
 @Data
-public class TestClass {
-    private List<String> consultantsLit = new ArrayList<>();
+public class NotionResponse {
+    private List<String> consultantsList = new ArrayList<>();
 
     @SuppressWarnings("unchecked")
     @JsonProperty("results")
     private void unpackNested(List<Map<String, Object>> results) {
-        /*
-        *   results <String, Object> ->
-        *   properties <String, Object> ->
-            Names <String, Object> ->
-            title <String, Object> ->
-            plain_text <String, String>
-     */
-        consultantsLit = results.stream()
+
+        consultantsList = results.stream()
+                .filter(p -> {
+                    Map<String, Object> properties = (Map<String, Object>)p.get("properties");
+                    Map<String, Object> names = (Map<String, Object>) properties.get("Name");
+                    List<Map<String, Object>> title = (List<Map<String, Object>>) names.get("title");
+                    return !title.isEmpty();
+                })
                 .map(p -> {
                     Map<String, Object> properties = (Map<String, Object>)p.get("properties");
                     Map<String, Object> names = (Map<String, Object>) properties.get("Name");
@@ -29,8 +29,5 @@ public class TestClass {
                     return (String) title.getFirst().get("plain_text");
                 })
                 .toList();
-
-//        Map<String,String> objectMap = (Map<String,String>)results.get("object");
-//        this.object = objectMap.get("object");
     }
 }
