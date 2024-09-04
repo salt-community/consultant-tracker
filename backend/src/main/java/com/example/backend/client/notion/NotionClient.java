a1d23470fb6c028d89d0374948aa278e6bba9b2a
kokakarova <kostadinkakarova@gmail.com>
2024-09-04 15:10:53 +0200

    BE - filter request with json string
    Co-authored-by: Anthony <raynaud.anthony@gmail.com>
    Co-authored-by: Mauro <mauroale2501@gmail.com>
    Co-authored-by: Jagoda <jagodabodnar90@gmail.com>

--- backend/src/main/java/com/example/backend/client/notion/NotionClient.java
+++ backend/src/main/java/com/example/backend/client/notion/NotionClient.java
@@ -1,25 +1,12 @@
 package com.example.backend.client.notion;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.util.JSONPObject;
 import lombok.Data;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.client.MultipartBodyBuilder;
 import org.springframework.stereotype.Service;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import static org.apache.logging.log4j.message.MapMessage.MapFormat.JSON;
 
 @Service
 @Data
@@ -48,29 +35,18 @@ public class NotionClient {
     public void getUsersFromNotion() {
 
         String requestBody = """
-            "filter": {
-              "property": "Responsible",
-              "people": {
-                "contains": "7f26998b-5296-439a-90ef-07dad83d3de2"
-             }
-        }""";
-
-        JSONObject jsonObj = new JSONObject(requestBody);
-
-        Map<String, String> contains = new HashMap<>();
-        contains.put("contains", "7f26998b-5296-439a-90ef-07dad83d3de2");
-        Map<String, String> people = new HashMap<>();
-        people.put("people", contains.toString());
-        Map<String, String> property = new HashMap<>();
-        property.put("property", "Responsible");
-        MultiValueMap<String, String> filter = new LinkedMultiValueMap<>();
-        filter.put("filter", List.of(property.toString(), people.toString()));
-
-        System.out.println("filter = " + filter);
+                    {
+                    "filter": {
+                      "property": "Responsible",
+                      "people": {
+                        "contains": "7f26998b-5296-439a-90ef-07dad83d3de2"
+                         }
+                     }
+                    }""";
 
         JsonNode dto = CLIENT_URL.post()
                 .uri("/{dbId}/query", DB_ID)
-                .body(jsonObj.toString(), String.class)
+                .bodyValue(requestBody)
                 .retrieve()
                 .bodyToMono(JsonNode.class)
                 .block();
