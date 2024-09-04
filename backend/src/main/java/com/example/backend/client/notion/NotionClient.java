4723411845d4ed1f248d6bd59f2739610cede497
kokakarova <kostadinkakarova@gmail.com>
2024-09-04 15:02:31 +0200

    BE - WIP add filter to post request
    Co-authored-by: Anthony <raynaud.anthony@gmail.com>
    Co-authored-by: Mauro <mauroale2501@gmail.com>
    Co-authored-by: Jagoda <jagodabodnar90@gmail.com>

--- backend/src/main/java/com/example/backend/client/notion/NotionClient.java
+++ backend/src/main/java/com/example/backend/client/notion/NotionClient.java
@@ -1,10 +1,25 @@
 package com.example.backend.client.notion;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import lombok.Data;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.client.MultipartBodyBuilder;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.apache.logging.log4j.message.MapMessage.MapFormat.JSON;
 
 @Service
 @Data
@@ -21,6 +36,7 @@ public class NotionClient {
                 .defaultHeaders(httpHeaders -> {
                     httpHeaders.set("Authorization", HEADER_AUTH);
                     httpHeaders.set("Notion-Version", "2022-06-28");
+                    httpHeaders.set("Content-Type", "application/json");
                 }).exchangeStrategies(ExchangeStrategies.builder()
                         .codecs(configurer -> configurer
                                 .defaultCodecs()
@@ -31,10 +47,32 @@ public class NotionClient {
 
     public void getUsersFromNotion() {
 
-        TestClass dto = CLIENT_URL.post()
+        String requestBody = """
+            "filter": {
+              "property": "Responsible",
+              "people": {
+                "contains": "7f26998b-5296-439a-90ef-07dad83d3de2"
+             }
+        }""";
+
+        JSONObject jsonObj = new JSONObject(requestBody);
+
+        Map<String, String> contains = new HashMap<>();
+        contains.put("contains", "7f26998b-5296-439a-90ef-07dad83d3de2");
+        Map<String, String> people = new HashMap<>();
+        people.put("people", contains.toString());
+        Map<String, String> property = new HashMap<>();
+        property.put("property", "Responsible");
+        MultiValueMap<String, String> filter = new LinkedMultiValueMap<>();
+        filter.put("filter", List.of(property.toString(), people.toString()));
+
+        System.out.println("filter = " + filter);
+
+        JsonNode dto = CLIENT_URL.post()
                 .uri("/{dbId}/query", DB_ID)
+                .body(jsonObj.toString(), String.class)
                 .retrieve()
-                .bodyToMono(TestClass.class)
+                .bodyToMono(JsonNode.class)
                 .block();
         System.out.println("dto = " + dto);
     }
--- backend/src/main/java/com/example/backend/client/notion/TestClass.java
+++ backend/src/main/java/com/example/backend/client/notion/TestClass.java
@@ -3,19 +3,33 @@ package com.example.backend.client.notion;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 @Data
 public class TestClass {
-//    private Map<String, Object> results = new HashMap<>();
-    private String object;
-//    private Map<String, Object> properties;
+    private List<String> consultantsLit = new ArrayList<>();
 
     @SuppressWarnings("unchecked")
     @JsonProperty("results")
-    private void unpackNested(List<Map<String,Object>> results) {
-        this.object = (String) results.get(0).get("object");
+    private void unpackNested(List<Map<String, Object>> results) {
+        /*
+        *   results <String, Object> ->
+        *   properties <String, Object> ->
+            Names <String, Object> ->
+            title <String, Object> ->
+            plain_text <String, String>
+     */
+        consultantsLit = results.stream()
+                .map(p -> {
+                    Map<String, Object> properties = (Map<String, Object>)p.get("properties");
+                    Map<String, Object> names = (Map<String, Object>) properties.get("Name");
+                    List<Map<String, Object>> title = (List<Map<String, Object>>) names.get("title");
+                    return (String) title.getFirst().get("plain_text");
+                })
+                .toList();
+
 //        Map<String,String> objectMap = (Map<String,String>)results.get("object");
 //        this.object = objectMap.get("object");
     }
