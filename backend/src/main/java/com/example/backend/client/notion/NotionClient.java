c6d001837054008e0655b851e0aa3737061fe23e
kokakarova <kostadinkakarova@gmail.com>
2024-09-04 13:37:44 +0200

    BE - create client class for notion
    Co-authored-by: Anthony <raynaud.anthony@gmail.com>
    Co-authored-by: Mauro <mauroale2501@gmail.com>
    Co-authored-by: Jagoda <jagodabodnar90@gmail.com>

---
+++ backend/src/main/java/com/example/backend/client/notion/NotionClient.java
@@ -0,0 +1,43 @@
+package com.example.backend.client.notion;
+
+import com.example.backend.client.notion.dto.NotionResponseDto;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.http.HttpHeaders;
+
+@Service
+@Data
+public class NotionClient {
+    private final WebClient CLIENT_URL;
+    private final String HEADER_AUTH;
+    private final String DB_ID;
+
+    public NotionClient(@Value("${NOTION.URL}") String baseUrl,
+                        @Value("${NOTION.AUTH}") String HEADER_AUTH,
+                        @Value("${NOTION.DB_ID}") String DB_ID) {
+        this.HEADER_AUTH = HEADER_AUTH;
+        CLIENT_URL = WebClient.builder().baseUrl(baseUrl)
+                .defaultHeaders(httpHeaders -> {
+                    httpHeaders.set("Authorization", HEADER_AUTH);
+                    httpHeaders.set("Notion-Version", "2022-06-28");
+                }).exchangeStrategies(ExchangeStrategies.builder()
+                        .codecs(configurer -> configurer
+                                .defaultCodecs()
+                                .maxInMemorySize(16 * 1024 * 1024))
+                        .build()).build();
+        this.DB_ID = DB_ID;
+    }
+
+    public void getUsersFromNotion() {
+
+        TestClass dto = CLIENT_URL.post()
+                .uri("/{dbId}/query", DB_ID)
+                .retrieve()
+                .bodyToMono(TestClass.class)
+                .block();
+        System.out.println("dto = " + dto);
+    }
+}
