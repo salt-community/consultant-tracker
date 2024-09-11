d9503ba68678b84946aaebe08aadc5ada0916cf9
kokakarova <kostadinkakarova@gmail.com>
2024-09-11 08:36:17 +0200

    BE - remove unused import statement
    Co-authored-by: Anthony <raynaud.anthony@gmail.com>
    Co-authored-by: Mauro <mauroale2501@gmail.com>
    Co-authored-by: Jagoda <jagodabodnar90@gmail.com>

--- backend/src/main/java/com/example/backend/consultant/ConsultantController.java
+++ backend/src/main/java/com/example/backend/consultant/ConsultantController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
-import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("/api/consultants")
