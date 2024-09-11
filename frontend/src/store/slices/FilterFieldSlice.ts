7c745b510f25ebe85da4d546518466c6da98254c
Mauro Morales <mauroale2501@gmail.com>
2024-09-11 15:10:48 +0200

    add includePgp filter to slicer
    co-authored-by: Anthony <raynaud.anthony@gmail.com>
    co-authored-by: Mauro <mauroale2501@gmail.com>
    co-authored-by: Jagoda <jagodabodnar90@gmail.com>
    co-authored-by: Koka <kostadinkakarova@gmail.com>

--- frontend/src/store/slices/FilterFieldSlice.ts
+++ frontend/src/store/slices/FilterFieldSlice.ts
@@ -1,48 +1,60 @@
-import {createSlice, PayloadAction} from "@reduxjs/toolkit";
-import {user} from "@/utils/utils";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface FilterFieldState {
-  filterClients: string[],
-  filterName: string,
-  debounceFilterName: string,
-  filterPts: string[],
-  listOfPts: string[],
-  listOfClients: string[]
+  filterClients: string[];
+  filterName: string;
+  debounceFilterName: string;
+  filterPts: string[];
+  listOfPts: string[];
+  listOfClients: string[];
+  includePgps: boolean;
 }
 
 const initialState: FilterFieldState = {
   filterClients: [],
   filterName: "",
   debounceFilterName: "",
-  filterPts: [user],
+  filterPts: [],
   listOfClients: [],
-  listOfPts: []
-}
+  listOfPts: [],
+  includePgps: false,
+};
 
 const filterFieldSlice = createSlice({
-  name: 'filterField',
+  name: "filterField",
   initialState,
   reducers: {
     setFilterClients: (state, action: PayloadAction<string[]>) => {
-      state.filterClients = action.payload
+      state.filterClients = action.payload;
     },
     setFilterPts: (state, action: PayloadAction<string[]>) => {
-      state.filterPts = action.payload
+      state.filterPts = action.payload;
     },
     setFilterName: (state, action: PayloadAction<string>) => {
-      state.filterName = action.payload
+      state.filterName = action.payload;
     },
     setListOfPts: (state, action: PayloadAction<string[]>) => {
-      state.listOfPts = action.payload
+      state.listOfPts = action.payload;
     },
     setListOfClients: (state, action: PayloadAction<string[]>) => {
-      state.listOfClients = action.payload
+      state.listOfClients = action.payload;
     },
-    setDebounceFilterName:(state, action: PayloadAction<string>) => {
-      state.debounceFilterName = action.payload
+    setDebounceFilterName: (state, action: PayloadAction<string>) => {
+      state.debounceFilterName = action.payload;
     },
-  }
-})
+    setIncludePgps: (state, action: PayloadAction<boolean>) => {
+      state.includePgps = action.payload;
+    },
+  },
+});
 
-export const {setFilterClients,setDebounceFilterName, setListOfClients, setListOfPts,setFilterPts, setFilterName} = filterFieldSlice.actions;
+export const {
+  setFilterClients,
+  setDebounceFilterName,
+  setListOfClients,
+  setListOfPts,
+  setFilterPts,
+  setFilterName,
+  setIncludePgps,
+} = filterFieldSlice.actions;
 export default filterFieldSlice.reducer;
