bef7d0d46ce60dda52b6f38b5c15971592c7dd45
Mauro Morales <mauroale2501@gmail.com>
2024-09-13 11:15:20 +0200

    change to NEXT_PUBLIC_BACKEND_URL
    co-authored-by: Anthony <raynaud.anthony@gmail.com>
    co-authored-by: Mauro <mauroale2501@gmail.com>
    co-authored-by: Jagoda <jagodabodnar90@gmail.com>
    co-authored-by: Koka <kostadinkakarova@gmail.com>

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.BACKEND_URL;
+const BASE_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
 export const getConsultantsData = async (page:number, pageSize:number, clientEncodeURI:string, ptsEncodeURI:string, filterName:string, includePgps: boolean) => {
   return await fetch(
