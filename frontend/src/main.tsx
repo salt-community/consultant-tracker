89d1d3ab70f6d7308f759ed1e2c251c2127a0d7b
kokakarova <kostadinkakarova@gmail.com>
2024-09-16 17:02:04 +0200

    FE - update imports in main.tsx
    Co-authored-by: Anthony <raynaud.anthony@gmail.com>
    Co-authored-by: Mauro <mauroale2501@gmail.com>
    Co-authored-by: Jagoda <jagodabodnar90@gmail.com>

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,11 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import React from "react";
-import { ClerkProvider } from "@clerk/clerk-react";
-import { RouterProvider } from "react-router-dom";
+
+
 import { router } from "./routing/routing";
+import { RouterProvider } from "react-router-dom";
+import { ClerkProvider } from "@clerk/clerk-react";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 if (!PUBLISHABLE_KEY) {
