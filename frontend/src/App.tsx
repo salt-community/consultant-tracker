47e0cdcd7d8cd36227ffad31587e7659b30e1261
JagodaBodnar <jagodabodnar90@gmail.com>
2024-09-17 16:53:26 +0200

    fix build
    co-authored-by: Anthony <raynaud.anthony@gmail.com>
    co-authored-by: Mauro <mauroale2501@gmail.com>
    co-authored-by: Jagoda <jagodabodnar90@gmail.com>
    co-authored-by: Koka <kostadinkakarova@gmail.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,11 @@
-import { useUser } from "@clerk/clerk-react";
+// import { useUser } from "@clerk/clerk-react";
 import "./globals.css";
 import Home from "./view/home/home";
-import LogIn from "./view/sign-in/sign-in";
+// import LogIn from "./view/sign-in/sign-in";
 
 const App = ()=> {
 
-  const { user } = useUser();
+  // const { user } = useUser();
   // return <>{!user ? <LogIn /> : <Home />}</>;
   return <Home/>
 }
