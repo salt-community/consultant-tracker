9c9badb32fcfed4a0f28d1000807333975ca8eb4
JagodaBodnar <jagodabodnar90@gmail.com>
2024-09-17 16:40:05 +0200

    test with signIn component
    co-authored-by: Anthony <raynaud.anthony@gmail.com>
    co-authored-by: Mauro <mauroale2501@gmail.com>
    co-authored-by: Jagoda <jagodabodnar90@gmail.com>
    co-authored-by: Koka <kostadinkakarova@gmail.com>

--- frontend/src/components/page-not-found/page-not-found.tsx
+++ frontend/src/components/page-not-found/page-not-found.tsx
@@ -1,7 +1,11 @@
 import './page-not-found.css'
+import SignIn from "../../view/sign-in/sign-in.tsx";
 
 export const PageNotFound = () => {
   return (
+    <>
+      <SignIn />
       <span className="page-not-found">Page not found. Please check the url and try again.</span>
+    </>
   );
 };
