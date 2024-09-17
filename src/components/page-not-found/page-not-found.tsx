25c3c4506f0ae72df42e394b2ca0d8913eccb7af
JagodaBodnar <JagodaBodnar@users.noreply.github.com>
2024-09-17 14:29:36 +0000

    deploy: 25855d1af4ec62a03ef50a15d2a93de94e03b3fd

--- src/components/page-not-found/page-not-found.tsx
+++
@@ -1,7 +0,0 @@
-import './page-not-found.css'
-
-export const PageNotFound = () => {
-  return (
-      <span className="page-not-found">Page not found. Please check the url and try again.</span>
-  );
-};
--- src/store/store-provider.tsx
+++
@@ -1,8 +0,0 @@
-import { Provider } from 'react-redux'
-import { store } from './store';
-
-function StoreProvider({children}: {children: React.ReactNode}) {
-  return <Provider store={store}> {children} </Provider>
-}
-
-export default StoreProvider;
