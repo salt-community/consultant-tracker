50ca1c84795a6eadfea3f378ef70b8504cf5e62d
JagodaBodnar <jagodabodnar90@gmail.com>
2024-09-18 12:40:05 +0200

    revert 404 test
    co-authored-by: Anthony <raynaud.anthony@gmail.com>
    co-authored-by: Mauro <mauroale2501@gmail.com>
    co-authored-by: Jagoda <jagodabodnar90@gmail.com>
    co-authored-by: Koka <kostadinkakarova@gmail.com>

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/Consultant-Tracker/',
-  build: {
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html'),
-        404: path.resolve(__dirname, '404.html'),
-      },
-    },
-  },
 })
