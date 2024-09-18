d501e65cdb264ddba667052dcc9e3cc43c25dd99
JagodaBodnar <jagodabodnar90@gmail.com>
2024-09-18 12:18:16 +0200

    update vite config
    co-authored-by: Anthony <raynaud.anthony@gmail.com>
    co-authored-by: Mauro <mauroale2501@gmail.com>
    co-authored-by: Jagoda <jagodabodnar90@gmail.com>
    co-authored-by: Koka <kostadinkakarova@gmail.com>

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/Consultant-Tracker/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        404: path.resolve(__dirname, 'public/404.html'),
+      },
+    },
+  },
 })
