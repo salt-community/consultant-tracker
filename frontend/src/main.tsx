d9292d2e741d12970f676d7fbf3abe5caa7c8430
JagodaBodnar <jagodabodnar90@gmail.com>
2024-09-18 10:37:41 +0200

    test RedirectToSignUp option from Clerk
    co-authored-by: Anthony <raynaud.anthony@gmail.com>
    co-authored-by: Mauro <mauroale2501@gmail.com>
    co-authored-by: Jagoda <jagodabodnar90@gmail.com>
    co-authored-by: Koka <kostadinkakarova@gmail.com>

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,7 +5,7 @@ import React from "react";
 
 import { router } from "./routing/router.tsx";
 import { RouterProvider } from "react-router-dom";
-import {ClerkProvider} from "@clerk/clerk-react";
+import {ClerkProvider, RedirectToSignUp, SignedOut} from "@clerk/clerk-react";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -16,7 +16,12 @@ if (!PUBLISHABLE_KEY) {
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <RouterProvider router={router} />
+      {/*<RouterProvider router={router} />*/}
+      <SignedOut>
+        <RedirectToSignUp
+          signInFallbackRedirectUrl="/Consultant-Tracker/"
+        />
+      </SignedOut>
     </ClerkProvider>
   </React.StrictMode>
 );
