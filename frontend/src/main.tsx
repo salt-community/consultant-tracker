473f5ceed717427dd5eddc2946b2d487316a3e82
JagodaBodnar <jagodabodnar90@gmail.com>
2024-09-18 11:08:05 +0200

    test with Routing
    co-authored-by: Anthony <raynaud.anthony@gmail.com>
    co-authored-by: Mauro <mauroale2501@gmail.com>
    co-authored-by: Jagoda <jagodabodnar90@gmail.com>
    co-authored-by: Koka <kostadinkakarova@gmail.com>

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,10 +3,11 @@ import "./index.css";
 import React from "react";
 
 
-// import { router } from "./routing/router.tsx";
-// import { RouterProvider } from "react-router-dom";
+import { router } from "./routing/router.tsx";
+import { RouterProvider } from "react-router-dom";
 import {ClerkProvider, RedirectToSignIn, SignedIn, SignedOut} from "@clerk/clerk-react";
 import App from "./App.tsx";
+import Home from "./view/home/home.tsx";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -17,8 +18,8 @@ if (!PUBLISHABLE_KEY) {
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      {/*<RouterProvider router={router} />*/}
-      <SignedIn><App /></SignedIn>
+      <RouterProvider router={router} />
+      <SignedIn><Home/></SignedIn>
       <SignedOut>
         <RedirectToSignIn signInForceRedirectUrl={"/Consultant-Tracker/"}/>
       </SignedOut>
