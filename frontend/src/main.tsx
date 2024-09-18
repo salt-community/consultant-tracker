67f81c7a56bf47f7bd84eb3f3353262e9ea34ab8
JagodaBodnar <jagodabodnar90@gmail.com>
2024-09-18 10:26:30 +0200

    update SignIn from redirect to path
    co-authored-by: Anthony <raynaud.anthony@gmail.com>
    co-authored-by: Mauro <mauroale2501@gmail.com>
    co-authored-by: Jagoda <jagodabodnar90@gmail.com>
    co-authored-by: Koka <kostadinkakarova@gmail.com>

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,7 +5,7 @@ import React from "react";
 
 import { router } from "./routing/router.tsx";
 import { RouterProvider } from "react-router-dom";
-import { ClerkProvider } from "@clerk/clerk-react";
+import {ClerkProvider} from "@clerk/clerk-react";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
