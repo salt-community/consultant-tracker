893ef0192eb77631855c214031314976c7ba76ac
JagodaBodnar <jagodabodnar90@gmail.com>
2024-09-18 15:03:38 +0200

    remove redundant imports and hooks
    co-authored-by: Anthony <raynaud.anthony@gmail.com>
    co-authored-by: Mauro <mauroale2501@gmail.com>
    co-authored-by: Jagoda <jagodabodnar90@gmail.com>
    co-authored-by: Koka <kostadinkakarova@gmail.com>

--- frontend/src/components/authentication/authentication.tsx
+++ frontend/src/components/authentication/authentication.tsx
@@ -1,13 +1,8 @@
 import {SignedIn, SignedOut} from "@clerk/clerk-react";
 import LogIn from "../../view/sign-in/sign-in.tsx";
 import Home from "../../view/home/home.tsx";
-import {useEffect, useState} from "react";
 
 const Authentication = () => {
-  const [authorized, setAuthorized] = useState(false);
-  useEffect(() => {
-
-  }, []);
   return (
     <>
       <SignedOut><LogIn/></SignedOut>
