9d7407ba9c20df29fba00dd151cfbca90e25304c
kokakarova <kostadinkakarova@gmail.com>
2024-09-20 09:54:52 +0200

    BE - remove unused imports
    Co-authored-by: Anthony <raynaud.anthony@gmail.com>
    Co-authored-by: Mauro <mauroale2501@gmail.com>
    Co-authored-by: Jagoda <jagodabodnar90@gmail.com>

--- backend/src/main/java/com/example/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/example/backend/config/SecurityConfig.java
@@ -6,14 +6,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.server.resource.web.authentication.BearerTokenAuthenticationFilter;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.access.intercept.AuthorizationFilter;
-import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -36,14 +32,10 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth ->
                         auth.anyRequest().authenticated())
                 .cors(withDefaults())
-//                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .oauth2ResourceServer(oauth2 -> {
-                    oauth2.jwt(withDefaults());
-                })
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()))
                 .addFilterAfter(new EmailAuthorizationFilter(AUTH_EMAILS), BearerTokenAuthenticationFilter.class)
                 .build();
     }
-
 
     @Bean
     public JwtDecoder jwtDecoder() {
