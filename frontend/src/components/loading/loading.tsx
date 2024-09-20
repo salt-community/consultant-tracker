bd46eaa33f5d898d08472b71321b99512224c00f
kokakarova <kostadinkakarova@gmail.com>
2024-09-20 15:29:57 +0200

    FE - add gif for loading
    Co-authored-by: Anthony <raynaud.anthony@gmail.com>
    Co-authored-by: Mauro <mauroale2501@gmail.com>
    Co-authored-by: Jagoda <jagodabodnar90@gmail.com>

--- frontend/src/components/loading/loading.tsx
+++ frontend/src/components/loading/loading.tsx
@@ -1,10 +1,12 @@
 import './loading.css'
 import {CircularProgress} from "@mui/material";
+import shaker from "../../../public/shaker.gif"
 
 const Loading = () => {
   return (
     <div className="spinner-container">
-    <CircularProgress disableShrink />
+      <img src={shaker} />
+    {/* <CircularProgress disableShrink /> */}
     </div>
   );
 };
