c743d0204561d2b33e90cdab4fc1cba034f88fe3
JagodaBodnar <jagodabodnar90@gmail.com>
2024-09-23 13:49:05 +0200

    FE - change image for unauthorized page
    co-authored-by: Anthony <raynaud.anthony@gmail.com>
    co-authored-by: Mauro <mauroale2501@gmail.com>
    co-authored-by: Jagoda <jagodabodnar90@gmail.com>
    co-authored-by: Koka <kostadinkakarova@gmail.com>

--- frontend/src/view/unauthorized/unauthorized.tsx
+++ frontend/src/view/unauthorized/unauthorized.tsx
@@ -1,17 +1,17 @@
 import './unauthorized.css'
 import {SignOutButton} from "@clerk/clerk-react";
 import Button from "@mui/material/Button";
-import unauthorizedImage from "../../assets/unauthorizedImage.png"
+import unauthorizedCat from "../../assets/unauthorizedCat.webp"
 
 const Unauthorized = () => {
   return (
     <div className="unauthorized-container">
-      <img src={unauthorizedImage} alt="unauthorized cat" className="unauthorized-image"/>
+      <img src={unauthorizedCat} alt="unauthorized cat" className="unauthorized-image"/>
       <div className="unauthorized-content-wrapper">
         <p>It seems like your account is not authorized to see this content.</p>
         <p>If you need access contact Salt admin and try again.</p>
+        <SignOutButton children={<Button variant="contained" color="primary">Try again</Button>}/>
       </div>
-      <SignOutButton children={<Button variant="contained" color="primary">Try again</Button>}/>
 
     </div>
   );
