5fa763b1ac7fbc13b9342ac6daf620d398ff2165
JagodaBodnar <jagodabodnar90@gmail.com>
2024-09-23 15:33:23 +0200

    FE - include scrollToTop button
    co-authored-by: Anthony <raynaud.anthony@gmail.com>
    co-authored-by: Mauro <mauroale2501@gmail.com>
    co-authored-by: Jagoda <jagodabodnar90@gmail.com>
    co-authored-by: Koka <kostadinkakarova@gmail.com>

--- frontend/src/view/home/home.tsx
+++ frontend/src/view/home/home.tsx
@@ -1,15 +1,36 @@
 import Dashboard from "../../components/dashboard/dashboard";
 import Navbar from "../../components/navbar/navbar";
 import "./home.css";
+import {useEffect, useState} from "react";
+import { BsArrowUp } from "react-icons/bs";
 
 const Home = () => {
-
+  const [isVisible, setIsVisible] = useState(false);
+  const handleScrollToTop = () => {
+    window.scrollTo({top: 0, behavior: "smooth"});
+  }
+  const toggleVisibility = () => {
+    if (window.scrollY > 300) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener('scroll', toggleVisibility);
+    return () => {
+      window.removeEventListener('scroll', toggleVisibility);
+    };
+  }, []);
   return (
     <>
-    <Navbar />
-    <div className="page-wrapper">
-      <Dashboard />
-    </div>
+      <button onClick={handleScrollToTop} className={isVisible ? "button-scroll-up" : "button-scroll-up hidden"}>
+        <BsArrowUp/>
+      </button>
+      <Navbar/>
+      <div className="page-wrapper">
+        <Dashboard/>
+      </div>
     </>
   );
 };
