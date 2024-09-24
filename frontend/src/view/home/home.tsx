4f54d882670eee46cf1e7819244a012ca4ef4f0c
JagodaBodnar <jagodabodnar90@gmail.com>
2024-09-24 11:30:41 +0200

    FE - add test indicator
    co-authored-by: Anthony <raynaud.anthony@gmail.com>
    co-authored-by: Mauro <mauroale2501@gmail.com>
    co-authored-by: Jagoda <jagodabodnar90@gmail.com>
    co-authored-by: Koka <kostadinkakarova@gmail.com>

--- frontend/src/view/home/home.tsx
+++ frontend/src/view/home/home.tsx
@@ -29,6 +29,7 @@ const Home = () => {
       </button>
       <Navbar/>
       <div className="dashboard-wrapper">
+        <div>Deployment test indicator</div>
         <Dashboard/>
       </div>
     </>
