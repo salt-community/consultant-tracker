c939129704b42fa9c32588fc6500758cda580006
JagodaBodnar <jagodabodnar90@gmail.com>
2024-09-26 16:07:35 +0200

    FE- add conditional button for admin to populateDB.
    co-authored-by: Anthony <raynaud.anthony@gmail.com>
    co-authored-by: Mauro <mauroale2501@gmail.com>
    co-authored-by: Jagoda <jagodabodnar90@gmail.com>
    co-authored-by: Koka <kostadinkakarova@gmail.com>

--- frontend/src/components/dashboard/dashboard.tsx
+++ frontend/src/components/dashboard/dashboard.tsx
@@ -1,17 +1,27 @@
 import "./dashboard.css";
 import FilterField from "../filter/filter";
 
-import {Provider} from "react-redux";
-import { store } from "../../store/store";
+import {Provider, useDispatch, useSelector} from "react-redux";
+import {RootState, store} from "../../store/store";
 import DashboardHeader from "./dashboard-header/dashboard-header";
 import GanttChart from "../gantt-chart/gantt-chart";
+import  {Toaster} from "react-hot-toast";
+import {setModalOpen} from "../../store/slices/ModalSlice.ts";
+import ModalComponent from "../modal-db/modal-db.tsx";
+import Button from "@mui/material/Button";
 
 const Dashboard = () => {
+  const role = useSelector((state: RootState) => state.authorization.role)
+  const dispatch = useDispatch();
+
   return (
     <Provider store={store}>
+      {role === "ADMIN" && <Button onClick={()=> dispatch(setModalOpen(true))} variant="contained" color="primary">Populate db</Button>}
+      <ModalComponent />
       <DashboardHeader/>
       <FilterField/>
       <GanttChart/>
+      <Toaster/>
     </Provider>
   );
 };
