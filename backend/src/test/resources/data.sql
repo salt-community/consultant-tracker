15d854d836042bc384e608abecf4d0d1b90ae243
kokakarova <kostadinkakarova@gmail.com>
2024-10-10 14:21:32 +0200

    BE - update test data.sql to latest table rows

--- backend/src/test/resources/data.sql
+++ backend/src/test/resources/data.sql
@@ -1,6 +1,6 @@
 INSERT INTO consultant (id, active, email, full_name, notion_id, timekeeper_id, client, country,
-                               salt_user_id)
+                               responsiblept_id)
 VALUES ('ab09d389-ff36-43ed-8b2b-4892438e46af', true, 'mock-email', 'mock-name', null, 2000, 'mock-client', 'Sverige',
         null);
 
-INSERT INTO salt_user (id, email, full_name, role) VALUES ('9b5dcb09-bd42-4306-a964-da0727c44a68', 'jane.doe@appliedtechnology.se','Jane Doe', 'PT' )
+INSERT INTO responsiblept (id, email, full_name, role) VALUES ('9b5dcb09-bd42-4306-a964-da0727c44a68', 'jane.doe@appliedtechnology.se','Jane Doe', 'PT' )
