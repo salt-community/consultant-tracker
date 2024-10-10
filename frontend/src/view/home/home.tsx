47a52676655fa45313a2ded4f72879988664b239
kokakarova <kostadinkakarova@gmail.com>
2024-10-10 14:45:18 +0200

    add min-height 100vh for dashboard-wrapper

--- frontend/src/view/home/home.tsx
+++ frontend/src/view/home/home.tsx
@@ -1,13 +1,13 @@
-import {Navbar, Dashboard} from "../../components";
+import { Navbar, Dashboard } from "../../components";
 import "./home.css";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import { BsArrowUp } from "react-icons/bs";
 
 export const Home = () => {
   const [isVisible, setIsVisible] = useState(false);
   const handleScrollToTop = () => {
-    window.scrollTo({top: 0, behavior: "smooth"});
-  }
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
   const toggleVisibility = () => {
     if (window.scrollY > 300) {
       setIsVisible(true);
@@ -16,19 +16,22 @@ export const Home = () => {
     }
   };
   useEffect(() => {
-    window.addEventListener('scroll', toggleVisibility);
+    window.addEventListener("scroll", toggleVisibility);
     return () => {
-      window.removeEventListener('scroll', toggleVisibility);
+      window.removeEventListener("scroll", toggleVisibility);
     };
   }, []);
   return (
     <>
-      <button onClick={handleScrollToTop} className={isVisible ? "button-scroll-up" : "button-scroll-up hidden"}>
-        <BsArrowUp/>
+      <button
+        onClick={handleScrollToTop}
+        className={isVisible ? "button-scroll-up" : "button-scroll-up hidden"}
+      >
+        <BsArrowUp />
       </button>
-      <Navbar/>
+      <Navbar />
       <div className="dashboard-wrapper">
-        <Dashboard/>
+        <Dashboard />
       </div>
     </>
   );
