c6b03e4962d88fcefb407f4defc7b8440bfe779f
Sabine <sabinehernandes@gmail.com>
2025-01-13 14:15:35 +0100

    FE - Add icon to refresh db button
    Co-authored-by: Sabine Hernandes <sabinehernandes@gmail.com>
    Co-authored-by: Sebastian Österberg <sebastian.osterberg@hotmail.se>
    Co-authored-by: Alexander Gabor <gaboralexanderalex@gmail.com>
    Co-authored-by: Andreas Lundmark <andreas.erik.lundmark@gmail.com>

--- frontend/src/components/dashboard/dashboard.tsx
+++ frontend/src/components/dashboard/dashboard.tsx
@@ -1,28 +1,39 @@
 import "./dashboard.css";
-import {FilterField, GanttChart, ModalComponent} from "../../components";
-import {Provider, useDispatch, useSelector} from "react-redux";
-import {RootState, store} from "../../store/store";
+import { FilterField, GanttChart, ModalComponent } from "../../components";
+import { Provider, useDispatch, useSelector } from "react-redux";
+import { RootState, store } from "../../store/store";
 import DashboardHeader from "./dashboard-header/dashboard-header";
-import {Toaster} from "react-hot-toast";
-import {setModalOpen} from "../../store/slices/ModalSlice.ts";
+import { Toaster } from "react-hot-toast";
+import { setModalOpen } from "../../store/slices/ModalSlice.ts";
 import Button from "@mui/material/Button";
+import RefreshIcon from "@mui/icons-material/Refresh";
 
 export const Dashboard = () => {
-  const role = useSelector((state: RootState) => state.authorization.role)
+  const role = useSelector((state: RootState) => state.authorization.role);
   const dispatch = useDispatch();
 
   return (
     <Provider store={store}>
-      {role === "ADMIN" &&
-          <Button onClick={() => dispatch(setModalOpen(true))} variant="contained" color="primary" className="button-wrapper" style={{ backgroundColor: '#D3D3D3', color: 'black', fontWeight: 300,  }}>
-              Refresh database
-          </Button>
-      }
-      <ModalComponent/>
-      <DashboardHeader/>
-      <FilterField/>
-      <GanttChart/>
-      <Toaster/>
+      {role === "ADMIN" && (
+        <Button
+          onClick={() => dispatch(setModalOpen(true))}
+          variant="contained"
+          color="primary"
+          className="button-wrapper"
+          style={{
+            backgroundColor: "#D3D3D3",
+            color: "black",
+            fontWeight: 300,
+          }}
+        >
+          Refresh database <RefreshIcon />
+        </Button>
+      )}
+      <ModalComponent />
+      <DashboardHeader />
+      <FilterField />
+      <GanttChart />
+      <Toaster />
     </Provider>
   );
 };
