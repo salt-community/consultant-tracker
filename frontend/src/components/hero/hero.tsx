67ba0bd8e57693520382f2bb9a9919ea08a07aa3
Sebastian Österberg <sebastian.osterberg@appliedtechnology.se>
2025-01-15 10:37:41 +0100

    Added Hero folder/component
    Co-authored-by: Sabine Hernandes <sabinehernandes@gmail.com>
    Co-authored-by: Sebastian Österberg <sebastian.osterberg@hotmail.se>
    Co-authored-by: Alexander Gabor <gaboralexanderalex@gmail.com>
    Co-authored-by: Andreas Lundmark <andreas.erik.lundmark@gmail.com>

---
+++ frontend/src/components/hero/hero.tsx
@@ -0,0 +1,11 @@
+import './hero.css'
+
+const Hero = () => {
+    return (
+        <div className="hero-container">
+            <h1 className="hero-header">&lt;Consultant tracker/&gt;</h1>
+        </div>
+    )
+}
+
+export default Hero;
