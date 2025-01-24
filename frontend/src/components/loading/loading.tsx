d4ff833bdf9161e7cc722950ddc3c5d249029ecc
Sabine <sabinehernandes@gmail.com>
2025-01-24 14:59:41 +0100

    fe - remove cat loader and replace with a css spin loader
    Co-authored-by: Sabine Hernandes <sabinehernandes@gmail.com>
    Co-authored-by: Sebastian Österberg <sebastian.osterberg@hotmail.se>
    Co-authored-by: Alexander Gabor <gaboralexanderalex@gmail.com>
    Co-authored-by: Andreas Lundmark <andreas.erik.lundmark@gmail.com>

--- frontend/src/components/loading/loading.tsx
+++ frontend/src/components/loading/loading.tsx
@@ -1,20 +1,5 @@
-import './loading.css'
-import Lottie from "react-lottie";
-import animationData from "../../lotties/cat-loading.json";
+import "./loading.css";
 
 export const Loading = () => {
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: animationData,
-  };
-  return (
-    <div className="spinner-container">
-      <Lottie
-        options={defaultOptions}
-        height={400}
-        width={400}
-      />
-    </div>
-  );
+  return <div className="loader"></div>;
 };
