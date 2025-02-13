0118ac90378b877bb58f3307fb4233d167db55cf
Sabine <sabinehernandes@gmail.com>
2025-02-13 14:42:21 +0100

    be - add swagger documentation for consultant controller
    Co-authored-by: Sebastian Österberg <sebastian.osterberg@hotmail.se>
    Co-authored-by: Andreas Lundmark <andreas.erik.lundmark@gmail.com>

--- backend/src/main/java/salt/consultanttracker/api/consultant/ConsultantController.java
+++ backend/src/main/java/salt/consultanttracker/api/consultant/ConsultantController.java
@@ -1,11 +1,13 @@
 package salt.consultanttracker.api.consultant;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import salt.consultanttracker.api.consultant.dto.*;
 import salt.consultanttracker.api.demo.DemoConsultantService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.UUID;
 
@@ -13,6 +15,7 @@ import java.util.UUID;
 @RequestMapping("/api/consultants")
 @RequiredArgsConstructor
 @CrossOrigin
+@Tag(name = "Consultants")
 public class ConsultantController {
 
     private final ConsultantService consultantService;
@@ -21,6 +24,13 @@ public class ConsultantController {
     private String appMode;
 
     @GetMapping
+    @Operation(description = "Returns all consultants and their registered working hours")
+    @Parameter(name = "page", description = "Page", required = true)
+    @Parameter(name = "pageSize", description = "How many consultants per page", required = true)
+    @Parameter(name = "name", description = "Consultant's name")
+    @Parameter(name = "client", description = "The client the consultant's assigned to")
+    @Parameter(name = "pt", description = "Responsible person from P&T team")
+    @Parameter(name = "includePgps", description = "Include people on PGP (waiting for assignment)")
     public ResponseEntity<ConsultantResponseListDto> getConsultantsAndRegisteredTime(
             @RequestParam(name = "page", defaultValue = "0") int page,
             @RequestParam(name = "pageSize", defaultValue = "10") int pageSize,
@@ -36,11 +46,15 @@ public class ConsultantController {
         return ResponseEntity.ok(consultantsResponse);
     }
 
+    @Operation(description = "Returns all consultants assigned to a specific P&T person")
+    @Parameter(name = "pt", description = "P&T person's name")
     @GetMapping("/infographics/{pt}")
     public ResponseEntity<InfographicResponseDto> getInfographicsByPt(@PathVariable String pt) {
         return ResponseEntity.ok(consultantService.getInfographicsByPt(pt));
     }
 
+    @Operation(description = "Returns a single consultant by their ID")
+    @Parameter(name = "id", description = "Consultant's ID")
     @GetMapping("/{id}")
     public ResponseEntity<SingleConsultantResponseListDto> getConsultantById(@PathVariable UUID id) {
         if ("demo".equalsIgnoreCase(appMode)) {
@@ -49,6 +63,8 @@ public class ConsultantController {
         return ResponseEntity.ok(consultantService.getConsultantById(id));
     }
 
+    @Operation(description = "Returns all clients and all people from P&T team")
+    @Parameter(name = "includePgps", description = "Include people on PGP (waiting for assignment)")
     @GetMapping("/all-clients-and-pts")
     public ResponseEntity<ClientsAndPtsListDto> getAllClientsAndPts(
             @RequestParam(defaultValue = "false", required = false) boolean includePgps
