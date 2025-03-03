12cb82c9edf917ae6422ebf4e0330c7fd4ea5a65
Sabine <sabinehernandes@gmail.com>
2025-03-03 11:19:05 +0100

    fe - fix typo on P&T
    Co-authored-by: Sebastian Österberg <sebastian.osterberg@hotmail.se>
    Co-authored-by: Andreas Lundmark <andreas.erik.lundmark@gmail.com>

--- frontend/src/components/consultant-detail/personal-data/personal-data.tsx
+++ frontend/src/components/consultant-detail/personal-data/personal-data.tsx
@@ -1,23 +1,28 @@
 import "./personal-data.css";
-import {useSelector} from "react-redux";
-import toast, {Toaster} from "react-hot-toast";
-import {SingleDetailField} from "../../../components";
+import { useSelector } from "react-redux";
+import toast, { Toaster } from "react-hot-toast";
+import { SingleDetailField } from "../../../components";
 import { RootState } from "../../../store/store";
 
-
 export const PersonalData = () => {
-  const personalData = useSelector((state: RootState) => state.basicInfo.personalData)
-  const {email, client, totalDaysStatistics, responsiblePt} = personalData!;
+  const personalData = useSelector(
+    (state: RootState) => state.basicInfo.personalData
+  );
+  const { email, client, totalDaysStatistics, responsiblePt } = personalData!;
   const handleEmailCopy = () => {
-    void navigator.clipboard.writeText(email)
-    toast.success("Email copied")
-  }
+    void navigator.clipboard.writeText(email);
+    toast.success("Email copied");
+  };
   return (
     <div className="personal-data__container">
       <div className="personal-data__info">
-        <SingleDetailField title="Email @appliedtechnology.se" content={email.split("@")[0]} onClick={handleEmailCopy}/>
-        <SingleDetailField title="Client" content={client}/>
-        <SingleDetailField title="Responsible pt" content={responsiblePt}/>
+        <SingleDetailField
+          title="Email @appliedtechnology.se"
+          content={email.split("@")[0]}
+          onClick={handleEmailCopy}
+        />
+        <SingleDetailField title="Client" content={client} />
+        <SingleDetailField title="Responsible P&T" content={responsiblePt} />
       </div>
       <div className="personal-data__statistics">
         <div className="personal-data__statistics-wrapper">
@@ -37,19 +42,19 @@ export const PersonalData = () => {
           <div className="personal-data_statistics-days">
             <h4>Total Hours</h4>
             <div className="personal-data_statistics__content">
-            <SingleDetailField
-              title="Worked"
-              content={`${totalDaysStatistics.totalWorkedHours}`}
-            />
-            <SingleDetailField
-              title="Remaining"
-              content={`${totalDaysStatistics.totalRemainingHours}`}
-            />
+              <SingleDetailField
+                title="Worked"
+                content={`${totalDaysStatistics.totalWorkedHours}`}
+              />
+              <SingleDetailField
+                title="Remaining"
+                content={`${totalDaysStatistics.totalRemainingHours}`}
+              />
             </div>
           </div>
         </div>
       </div>
-      <Toaster/>
+      <Toaster />
     </div>
   );
 };
